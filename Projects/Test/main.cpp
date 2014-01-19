c64b6416e4720fe8d88e36e965b71b74f5b57f7d
lhmouse <lh_mouse@126.com>
2014-01-19 22:57:23 +0800

    MCF Build binary files added(x86 and x64).

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,19 +1,19 @@
 #include <StdMCF.hpp>
-#include <Core/Thread.hpp>
+#include <Core/String.hpp>
 #include <cstdio>
 #include <cstdlib>
 
 unsigned int MCFMain(){
-	MCF::Thread thrd;
-	thrd.Start([]{
-		std::puts("in thread");
-		throw 12345;
-	});
-	try {
-		thrd.Join();
-		std::puts("no exception caught");
-	} catch(int e){
-		std::printf("exception caught, e = %d\n", e);
-	}
+	MCF::UTF16String s1, s2;
+	s1 = L"looooooooooooooooooooooooooooooooooooooooong";
+	s2 = L"meow";
+
+	std::puts("---------- before swap ----------");
+	s1.Swap(s2);
+	std::puts("---------- after swap ----------");
+
+	std::fputws(s1.GetCStr(), stdout);
+	std::fputws(s2.GetCStr(), stdout);
+
 	return 0;
 }
--- Projects/Test/make.cmd
+++ Projects/Test/make.cmd
@@ -10,4 +10,4 @@ if "%1"=="Release" (
 mcfbuild -p../../MCFCRT/MCFBuild.mcfproj -s../../MCFCRT -d.%Config%/mcfcrt -o.%Config%/libmcfcrt.a %*
 mcfbuild -p../../MCF/MCFBuild.mcfproj -s../../MCF -d.%Config%/mcf -o.%Config%/libmcf.a %*
 
-g++ %CXXFlags% -std=c++11 -Wall -Wextra main.cpp -o "a-%Config%.exe" -I../../MCFCRT -I../../MCF -L.%Config% -static -nostartfiles -Wl,-e__MCFExeStartup,--disable-runtime-pseudo-reloc,--disable-auto-import,-lmcf,-lmcfcrt,-lstdc++,-lgcc,-lgcc_eh,-lmingwex,-lmcfcrt
+g++ %CXXFlags% -std=c++11 -Wall -Wextra -pipe -mfpmath=sse,387 -msse2 -masm=intel main.cpp -o ".%Config%/a.exe" -I../../MCFCRT -I../../MCF -L.%Config% -static -nostartfiles -Wl,-e__MCFExeStartup,--disable-runtime-pseudo-reloc,--disable-auto-import,-lmcf,-lmcfcrt,-lstdc++,-lgcc,-lgcc_eh,-lmingwex,-lmcfcrt

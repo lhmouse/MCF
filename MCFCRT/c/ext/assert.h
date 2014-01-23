eb444fe68bf80d57f8ef8c5152f920f2df67a468
lhmouse <lh_mouse@126.com>
2014-01-23 23:28:21 +0800

    Assert bug fix.

--- MCFCRT/c/ext/assert.h
+++ MCFCRT/c/ext/assert.h
@@ -2,12 +2,12 @@
 // 有关具体授权说明，请参阅 MCFLicense.txt。
 // Copyleft 2013. LH_Mouse. All wrongs reserved.
 
-#undef ASSERT_MSG
+#undef __MCF_ASSERT_MSG
 
 #ifdef NDEBUG
-#	define ASSERT_MSG(exp, msg)		((void)0)
+#	define __MCF_ASSERT_MSG(plain, exp, msg)	((void)0)
 #else
-#	define ASSERT_MSG(exp, msg)		(!(exp) && (__MCF_CRT_OnAssertFail(L## #exp, __FILE__, __LINE__, (msg)), 1))
+#	define __MCF_ASSERT_MSG(plain, exp, msg)	(!(exp) && (__MCF_CRT_OnAssertFail(L##plain, __FILE__, __LINE__, (msg)), 1))
 #endif
 
 #ifndef __MCF_CRT_ASSERT_H__
@@ -16,28 +16,11 @@
 #include "../../env/_crtdef.h"
 #include "../../env/bail.h"
 
-static inline void __MCF_CRT_OnAssertFail(
-	const wchar_t *pwszExpression,
-	const char *pszFile,			// 这个使用系统缺省多字节编码。
-	__MCF_STD size_t uLine,
-	const wchar_t *pwszMessage
-){
-	__MCF_BailF(
-		L"调试断言失败。\n\n表达式：%ls\n文件　：%S\n行号　：%lu\n描述　：%ls",
-		pwszExpression,
-		pszFile,
-		(unsigned long)uLine,
-		pwszMessage
-	);
+static inline void __MCF_CRT_OnAssertFail(const wchar_t *pwszExpression, const char *pszFile, unsigned long ulLine, const wchar_t *pwszMessage){
+	__MCF_BailF(L"调试断言失败。\n\n表达式：%ls\n文件　：%hs\n行号　：%lu\n描述　：%ls", pwszExpression, pszFile, ulLine, pwszMessage);
 }
 
-#define ASSERT(exp)					ASSERT_MSG((exp), L"")
-#define BAIL_IF(exp)				ASSERT_MSG(!(exp), L"")
-#define BAIL_IF_MSG(exp, msg)		ASSERT_MSG(!(exp), (msg))
-
-#define ASSERT_TRUE(exp)			ASSERT(exp)
-#define ASSERT_TRUE_MSG(exp, msg)	ASSERT_MSG(exp, msg)
-#define ASSERT_FALSE(exp)			BAIL_IF(exp)
-#define ASSERT_FALSE_MSG(exp, msg)	BAIL_IF_MSG(exp, msg)
+#define ASSERT(exp)						__MCF_ASSERT_MSG(#exp, (exp), L"")
+#define ASSERT_MSG(exp, msg)			__MCF_ASSERT_MSG(#exp, (exp), (msg))
 
 #endif

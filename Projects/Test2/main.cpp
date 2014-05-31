ee5bd39c9ba7cfca36fca6753865860ea1321562
lhmouse <lh_mouse@126.com>
2014-05-31 18:32:39 +0800

    Updates.

--- Projects/Test2/main.cpp
+++ Projects/Test2/main.cpp
@@ -1,23 +1,36 @@
 #include <MCF/StdMCF.hpp>
 #include <MCF/Core/Exception.hpp>
-#include <MCF/Thread/Thread.hpp>
+#include <MCF/Socket/UdpServer.hpp>
 using namespace std;
 using namespace MCF;
 
 unsigned int MCFMain()
 try {
-	std::vector<std::shared_ptr<Thread>> v;
-	v.resize(200);
-	for(auto &p : v){
-		p = Thread::Create(
-			[]{
-				::Sleep(1000);
-				puts("meow"_AS.GetCStr());
-			}
-		);
+/*
+	auto svr = UdpServer::Create(PeerInfo(127,0,0,1, 10880));
+	for(;;){
+		auto pkt = svr->GetPacketTimeout(1000);
+		if(!pkt.pSender){
+			puts("listening...");
+			continue;
+		}
+
+		unsigned char ip[4];
+		unsigned short port;
+		pkt.pSender->GetPeerInfo().ToIPv4(ip, port);
+		printf("remote = %hhu.%hhu.%hhu.%hhu:%hu\n", ip[0], ip[1], ip[2], ip[3], port);
+		printf("packet size = %zu\n  contents: ", pkt.vecPayload.GetSize());
+		fwrite(pkt.vecPayload.GetData(), pkt.vecPayload.GetSize(), 1, stdout);
+		putchar('\n');
 	}
-	for(auto &p : v){
-		p->Join();
+*/
+	auto snd = UdpSender::Create(PeerInfo(239,122,33,45, 10880), PeerInfo(192,168,1,2, 20100));
+	for(;;){
+		::Sleep(1000);
+		char buf[20];
+		int cnt = snprintf(buf, sizeof(buf), "%d", rand());
+		snd->Send(buf, (unsigned)cnt);
+		printf("sent %s\n", buf);
 	}
 
 	return 0;
@@ -29,7 +42,7 @@ try {
 		printf("  desc = %s\n", AnsiString(GetWin32ErrorDesc(p->m_ulErrorCode)).GetCStr());
 		printf("  func = %s\n", p->m_pszFunction);
 		printf("  line = %lu\n", p->m_ulLine);
-		printf("  msg  = %s\n", AnsiString(p->m_wcsMessage).GetCStr());
+		printf("  msg  = %s\n", AnsiString(WideString(p->m_wcsMessage)).GetCStr());
 	}
 	return 0;
 }

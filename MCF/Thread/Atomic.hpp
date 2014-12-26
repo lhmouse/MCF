304da8db50de29105ed132a5da7dfec381464ce8
lhmouse <lh_mouse@126.com>
2014-12-27 00:45:35 +0800

    Minor.

--- MCF/Thread/Atomic.hpp
+++ MCF/Thread/Atomic.hpp
@@ -2,8 +2,8 @@
 // 有关具体授权说明，请参阅 MCFLicense.txt。
 // Copyleft 2013 - 2014, LH_Mouse. All wrongs reserved.
 
-#ifndef MCF_UTILITIES_ATOMIC_HPP_
-#define MCF_UTILITIES_ATOMIC_HPP_
+#ifndef MCF_THREAD_ATOMIC_HPP_
+#define MCF_THREAD_ATOMIC_HPP_
 
 #include <type_traits>
 

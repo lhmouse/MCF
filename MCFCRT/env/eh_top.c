2c6361845c1f3f3d0ff6caffd5d57d8c722e8de6
lhmouse <lh_mouse@126.com>
2015-06-26 17:44:08 +0800

    Updates.

--- MCFCRT/env/eh_top.c
+++ MCFCRT/env/eh_top.c
@@ -7,26 +7,10 @@
 #include "../env/module.h"
 #include "../ext/unref_param.h"
 
-static bool TraverseModuleSectionsCallback(intptr_t nContext, const char achName[8], void *pBase, size_t uSize){
-	UNREF_PARAM(uSize);
-
-	if(memcmp(achName, ".eh_fram", 8) == 0){
-		*(void **)nContext = pBase;
-		return false;
-	}
-	return true;
-}
-
 // 参见 gcc/libgcc/unwind-dw2-fde.h 里面的 old_object 的注释。
 struct object {
 	void *impl[6];
 };
-
-__extension__ __attribute__((__section__(".eh_fram$@@@"), __used__))
-static const uintptr_t _eh_begin[0] = { };
-
-static struct object _eh_obj;
-static void *_eh_frame_base = &_eh_begin;
 
 __attribute__((__weak__))
 void __register_frame_info(const void *p, struct object *o){
@@ -40,11 +24,39 @@ void *__deregister_frame_info(const void *p){
 	return nullptr;
 }
 
+__extension__ __attribute__((__section__(".eh_fram$@@@"), __used__))
+static const char _eh_begin[0] = { };
+
+static struct object _eh_obj;
+static void *_eh_frame_base;
+
+static bool TraverseModuleSectionsCallback(intptr_t nContext, const char achName[8], void *pBase, size_t uSize){
+	UNREF_PARAM(nContext);
+	UNREF_PARAM(achName);
+
+	if(((uintptr_t)pBase <= (uintptr_t)_eh_begin) && ((uintptr_t)_eh_begin < (uintptr_t)pBase + uSize)){
+		*(void **)nContext = pBase;
+		return false;
+	}
+	return true;
+}
+
 bool __MCF_CRT_RegisterFrameInfo(){
-	MCF_CRT_TraverseModuleSections(&TraverseModuleSectionsCallback, (intptr_t)&_eh_frame_base);
-	__register_frame_info(_eh_frame_base, &_eh_obj);
+	void *pBase = nullptr;
+	MCF_CRT_TraverseModuleSections(&TraverseModuleSectionsCallback, (intptr_t)&pBase);
+	if(!pBase){
+		return false;
+	}
+
+	__register_frame_info(pBase, &_eh_obj);
+	_eh_frame_base = pBase;
 	return true;
 }
 void __MCF_CRT_UnregisterFrameInfo(){
-	__deregister_frame_info(_eh_frame_base);
+	void *const pBase = _eh_frame_base;
+	_eh_frame_base = nullptr;
+
+	if(pBase){
+		__deregister_frame_info(pBase);
+	}
 }

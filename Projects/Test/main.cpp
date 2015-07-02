e574c02d8c9d20a552d808e90004fa480d92fdc6
lhmouse <lh_mouse@126.com>
2015-07-02 10:23:58 +0800

    TEST.

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,6 +1,7 @@
 #include <MCF/StdMCF.hpp>
 #include <MCF/Thread/Thread.hpp>
 #include <MCF/Thread/Mutex.hpp>
+#include <MCF/Core/Time.hpp>
 
 using namespace MCF;
 
@@ -8,18 +9,25 @@ Mutex m;
 volatile int c = 0;
 
 extern "C" unsigned MCFMain(){
-	std::array<IntrusivePtr<Thread>, 100> threads;
+	std::array<IntrusivePtr<Thread>, 4> threads;
 	for(auto &p : threads){
 		p = Thread::Create([]{
-			for(int i = 0; i < 10000; ++i){
+			for(int i = 0; i < 500000; ++i){
 				const Mutex::UniqueLock l(m);
 				++c;
 			}
-		});
+		}, true);
+	}
+
+	const auto t1 = GetHiResMonoClock();
+	for(auto &p : threads){
+		p->Resume();
 	}
 	for(auto &p : threads){
 		p->Join();
 	}
-	std::printf("c = %d\n", c);
+	const auto t2 = GetHiResMonoClock();
+
+	std::printf("c = %d, time = %f\n", c, t2 - t1);
 	return 0;
 }

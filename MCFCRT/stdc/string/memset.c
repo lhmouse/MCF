f422b4833db2e52b090880c5f0c3532b811ac1eb
lhmouse <lh_mouse@126.com>
2015-08-29 12:01:06 +0800

    Updates.

--- MCFCRT/stdc/string/memset.c
+++ MCFCRT/stdc/string/memset.c
@@ -5,57 +5,74 @@
 #include "../../env/_crtdef.h"
 #include "_string_asm.h"
 
+__attribute__((__force_align_arg_pointer__))
 void *memset(void *dst, int ch, size_t cb){
 	uintptr_t unused;
+	alignas(16) char xmmreg[16];
 	__asm__ __volatile__(
-		"mov " __RCX ", %5 \n"
-		"cmp " __RCX ", 16 \n"
-		"jb 1f  \n"
-#ifdef _WIN64
-		"test rdi, 7 \n"
-#else
-		"test edi, 3 \n"
-#endif
-		"jz 2f \n"
-		"	3: \n"
-		"	stosb \n"
-		"	dec %5 \n"
-#ifdef _WIN64
-		"	test rdi, 7 \n"
-#else
-		"	test edi, 3 \n"
-#endif
-		"	jnz 3b \n"
-		"2: \n"
-		"movzx " __RAX ", al \n"
-		"mov " __RCX ", " __RAX " \n"
-		"shl " __RCX ", 8 \n"
-		"or " __RAX ", " __RCX " \n"
-		"mov " __RCX ", " __RAX " \n"
-		"shl " __RCX ", 16 \n"
-		"or " __RAX ", " __RCX " \n"
+		"movzx eax, al \n"
+		"mov ecx, eax \n"
+		"shl ecx, 8 \n"
+		"or eax, ecx \n"
+		"mov ecx, eax \n"
+		"shl ecx, 16 \n"
+		"or eax, ecx \n"
 #ifdef _WIN64
 		"mov rcx, rax \n"
 		"shl rcx, 32 \n"
-		"or rax, rcx \n"
+		"or rcx, rcx \n"
 #endif
-		"mov " __RCX ", %5 \n"
+		"cmp %2, 64 \n"
+		"jb 1f \n"
+#ifdef _WIN64
+		"	mov qword ptr[%3], rax \n"
+		"	mov qword ptr[%3 + 8], rax \n"
+#else
+		"	mov dword ptr[%3], eax \n"
+		"	mov dword ptr[%3 + 4], eax \n"
+		"	mov dword ptr[%3 + 8], eax \n"
+		"	mov dword ptr[%3 + 12], eax \n"
+#endif
+		"	mov " __RCX ", " __RDI " \n"
+		"	neg " __RCX " \n"
+		"	and " __RCX ", 0x0F \n"
+		"	sub %2, " __RCX " \n"
+		"	movdqa xmm0, xmmword ptr[%3] \n"
+		"	rep stosb \n"
+		"	mov " __RCX ", %2 \n"
+		"	shr " __RCX ", 4 \n"
+		"	and %2, 0x0F \n"
+		"	cmp " __RCX ", 64 * 1024 * 16 \n" // 16 MiB
+		"	jb 4f \n"
+		"		3: \n"
+		"		dec " __RCX " \n"
+		"		movntdq xmmword ptr[" __RDI "], xmm0 \n"
+		"		lea " __RDI ", dword ptr[" __RDI " + 16] \n"
+		"		jnz 3b \n"
+		"		jmp 1f \n"
+		"	4: \n"
+		"	3: \n"
+		"	dec " __RCX " \n"
+		"	movdqa xmmword ptr[" __RDI "], xmm0 \n"
+		"	lea " __RDI ", dword ptr[" __RDI " + 16] \n"
+		"	jnz 3b \n"
+		"1: \n"
+		"mov " __RCX ", %2 \n"
 #ifdef _WIN64
 		"shr rcx, 3 \n"
 		"rep stosq \n"
-		"mov rcx, %5 \n"
+		"mov rcx, %2 \n"
 		"and rcx, 7 \n"
 #else
 		"shr ecx, 2 \n"
 		"rep stosd \n"
-		"mov ecx, %5 \n"
+		"mov ecx, %2 \n"
 		"and ecx, 3 \n"
 #endif
-		"1: \n"
 		"rep stosb \n"
-		: "=D"(unused), "=a"(unused), "=r"(unused)
+		: "=D"(unused), "=a"(unused), "=r"(unused), "=m"(xmmreg)
 		: "0"(dst), "1"(ch), "2"(cb)
-		: "cx"
+		: "cx", "xmm0"
 	);
 	return dst;
 }
--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,6 +1,52 @@
 #include <MCF/StdMCF.hpp>
+#include <MCF/Core/Time.hpp>
+
+#include <MCFCRT/stdc/string/_string_asm.h>
+
+void rep_memset(void *dst, int ch, size_t cb){
+	__asm__ __volatile__(
+		"mov rcx, rax ;"
+		"shl rcx, 8 ;"
+		"or rax, rcx ;"
+		"mov rcx, rax ;"
+		"shl rcx, 16 ;"
+		"or rax, rcx ;"
+		"mov rcx, rax ;"
+		"shl rcx, 32 ;"
+		"or rax, rcx ;"
+		"mov rcx, rdx ;"
+		"shr rcx, 3 ;"
+		"rep stosq ;"
+		"mov rcx, rdx ;"
+		"and rcx, 7 ;"
+		"rep stosb ;"
+		: "+D"(dst)
+		: "a"(ch), "d"(cb)
+		: "cx"
+	);
+}
 
 extern "C" unsigned MCFMain(){
-	new int;
+	constexpr std::size_t clen = 0x10000 + 6;
+	constexpr std::size_t repeat = 100000;
+
+	std::unique_ptr<char []> ptr(new char[clen]);
+	std::memset(ptr.get(), 'a', clen);
+	double t1, t2;
+
+	t1 = MCF::GetHiResMonoClock();
+	for(unsigned i = 0; i < repeat; ++i){
+		rep_memset(ptr.get(), 'b', clen);
+	}
+	t2 = MCF::GetHiResMonoClock();
+	std::printf("rep_memset time = %f\n", t2 - t1);
+
+	t1 = MCF::GetHiResMonoClock();
+	for(unsigned i = 0; i < repeat; ++i){
+		std::memset(ptr.get(), 'c', clen);
+	}
+	t2 = MCF::GetHiResMonoClock();
+	std::printf("MCFCRT memset time = %f\n", t2 - t1);
+
 	return 0;
 }

c7f1999a8964b3167bacd184b19fd598ece77e20
lhmouse <lh_mouse@126.com>
2015-09-02 01:28:40 +0800

    Minor.

---
+++ MCFCRT/stdc/string/strcpy.c
@@ -0,0 +1,11 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2015, LH_Mouse. All wrongs reserved.
+
+#include "../../env/_crtdef.h"
+#include "../../ext/stpcpy.h"
+
+char *strcpy(char *s1, const char *s2){
+	MCF_stpcpy(s1, s2);
+	return s1;
+}
---
+++ MCFCRT/stdc/wchar/wcscpy.c
@@ -0,0 +1,11 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2015, LH_Mouse. All wrongs reserved.
+
+#include "../../env/_crtdef.h"
+#include "../../ext/wcpcpy.h"
+
+wchar_t *wcscpy(wchar_t *s1, const wchar_t *s2){
+	MCF_wcscpy(s1, s2);
+	return s1;
+}

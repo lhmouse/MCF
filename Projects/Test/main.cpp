b892e5ce634ab0000cee4b53b6301f3fbd725606
lhmouse <lh_mouse@126.com>
2015-09-11 13:42:35 +0800

    Minor.

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,14 +1,6 @@
-#include <MCF/Containers/StaticVector.hpp>
-#include <cstdio>
-
-using namespace MCF;
-
-template class StaticVector<int, 8>;
-
 extern "C" unsigned MCFMain(){
-	StaticVector<int, 8> a{0,1,2,3,4,5,6};
-	for(auto e : a){
-		std::printf("e = %d\n", e);
-	}
+	auto p = new int[5];
+	p[-1] = 0;
+	delete[] p;
 	return 0;
 }

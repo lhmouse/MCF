c14dad2ffc77339ba1a4207799b17e1cb6091ecb
lhmouse <lh_mouse@126.com>
2015-09-12 01:46:32 +0800

    Minor.

--- MCFCRT/exe/startup.c
+++ MCFCRT/exe/startup.c
@@ -99,23 +99,24 @@ static void TlsCallback(void *hModule, DWORD dwReason, void *pReserved){
 
 // 线程局部存储（TLS）目录，用于执行 TLS 的析构函数。
 __extension__ __attribute__((__section__(".tls$@@@"), __used__))
-static const char _tls_begin[0] = { };
+static const char _tls_start[0] = { };
 __extension__ __attribute__((__section__(".tls$___"), __used__))
 static const char _tls_end[0]   = { };
-__attribute__((__section__(".tls"), __used__))
-static const DWORD _tls_index   = 0;
 
 __attribute__((__section__(".CRT$@@@"), __used__))
-static const PIMAGE_TLS_CALLBACK _tls_callback_begin = &TlsCallback;
+static const PIMAGE_TLS_CALLBACK _callback_start = &TlsCallback;
 __attribute__((__section__(".CRT$___"), __used__))
-static const PIMAGE_TLS_CALLBACK _tls_callback_end   = nullptr;
+static const PIMAGE_TLS_CALLBACK _callback_end   = nullptr;
+
+__attribute__((__section__(".tls"), __used__))
+const DWORD _tls_index   = 0;
 
 __attribute__((__section__(".tls"), __used__))
 const IMAGE_TLS_DIRECTORY _tls_used = {
-	.StartAddressOfRawData = (UINT_PTR)&_tls_begin,
+	.StartAddressOfRawData = (UINT_PTR)&_tls_start,
 	.EndAddressOfRawData   = (UINT_PTR)&_tls_end,
 	.AddressOfIndex        = (UINT_PTR)&_tls_index,
-	.AddressOfCallBacks    = (UINT_PTR)&_tls_callback_begin,
+	.AddressOfCallBacks    = (UINT_PTR)&_callback_start,
 	.SizeOfZeroFill        = 0,
 	.Characteristics       = 0,
 };
--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,8 +1,3 @@
 extern "C" unsigned MCFMain(){
-	try {
-		throw 123;
-	} catch(int e){
-		__builtin_printf("exception: e = %d\n", e);
-	}
 	return 0;
 }

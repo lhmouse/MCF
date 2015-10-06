e01a51c49ad98b614fb76dc3244f88349ef99140
lhmouse <lh_mouse@126.com>
2015-10-07 02:19:11 +0800

    Updates.

--- MCF/Thread/ReaderWriterMutex.cpp
+++ MCF/Thread/ReaderWriterMutex.cpp
@@ -7,6 +7,7 @@
 #include "../Core/Exception.hpp"
 #include "../Core/System.hpp"
 #include "../Utilities/MinMax.hpp"
+#include "Thread.hpp"
 
 namespace MCF {
 
@@ -70,7 +71,7 @@ void ReaderWriterMutex::LockAsReader() noexcept {
 
 	const auto uSpinCount = GetSpinCount() / 0x400; // FIXME: SRWLOCK 里面自己实现了一个自旋。
 	for(std::size_t i = 0; i < uSpinCount; ++i){
-		::SwitchToThread();
+		Thread::YieldExecution();
 
 		if(::TryAcquireSRWLockShared(reinterpret_cast<::SRWLOCK *>(x_aImpl))){
 			return;
@@ -93,7 +94,7 @@ void ReaderWriterMutex::LockAsWriter() noexcept {
 
 	const auto uSpinCount = GetSpinCount() / 0x400; // FIXME: SRWLOCK 里面自己实现了一个自旋。
 	for(std::size_t i = 0; i < uSpinCount; ++i){
-		::SwitchToThread();
+		Thread::YieldExecution();
 
 		if(::TryAcquireSRWLockExclusive(reinterpret_cast<::SRWLOCK *>(x_aImpl))){
 			return;

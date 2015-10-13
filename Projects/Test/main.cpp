fee53d4b8ad4fd0c85ce4432e3a2b4a522b12ca2
lhmouse <lh_mouse@126.com>
2015-10-14 01:32:32 +0800

    TEST

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,7 +1,6 @@
 #include <MCF/StdMCF.hpp>
 #include <MCF/Thread/Thread.hpp>
 #include <MCF/Thread/Mutex.hpp>
-#include <MCF/Thread/ConditionVariable.hpp>
 #include <MCF/Core/Time.hpp>
 
 using namespace MCF;
@@ -9,27 +8,20 @@ using namespace MCF;
 Mutex m;
 volatile int c = 0;
 
-ConditionVariable cv;
-
 extern "C" unsigned MCFMain(){
 	std::array<IntrusivePtr<Thread>, 4> threads;
 	for(auto &p : threads){
 		p = Thread::Create([]{
-			std::puts("sleep on cv!");
-			Mutex::UniqueLock l(m);
-			cv.Wait(l);
-			std::puts("wake up!");
+			for(int i = 0; i < 1000000; ++i){
+				const auto l = m.GetLock();
+				++c;
+			}
 		}, true);
 	}
 
 	const auto t1 = GetHiResMonoClock();
 	for(auto &p : threads){
 		p->Resume();
-	}
-	for(int i = 0; i < 10; ++i){
-		::Sleep(1000);
-		std::puts("- signal one!");
-		cv.Signal();
 	}
 	for(auto &p : threads){
 		p->Join();

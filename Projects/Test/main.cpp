174f786c17079b8c2ae58c9c125983ffc23718a1
lhmouse <lh_mouse@126.com>
2015-12-31 15:29:54 +0800

    Bug fix.

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,38 +1,13 @@
 #include <MCF/StdMCF.hpp>
 #include <MCF/Function/Function.hpp>
+#include <MCF/Function/Bind.hpp>
 
 using namespace MCF;
 
-struct foo {
-	foo(){
-		std::puts(__PRETTY_FUNCTION__);
-	}
-	foo(const foo &){
-		std::puts(__PRETTY_FUNCTION__);
-	}
-	foo(foo &&) noexcept {
-		std::puts(__PRETTY_FUNCTION__);
-	}
-	~foo(){
-		std::puts(__PRETTY_FUNCTION__);
-	}
-};
-
-struct bar {
-	void operator()(foo) const {
-		std::puts(__PRETTY_FUNCTION__);
-	}
-};
-
 extern "C" unsigned MCFMain(){
-	std::puts("--- test 1 ---");
-	Function<void (foo)> f;
-	f.Reset(bar());
-	f(foo());
-
-	std::puts("--- test 2 ---");
-	bar b;
-	b(foo());
+	Function<void(int, int)> f;
+//	f = [](int){};
+	f = Bind([](int){}, _1);
 
 	return 0;
 }

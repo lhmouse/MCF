7df0b162181a84240ba1b0b129d0cb6f03cd47e3
lhmouse <lh_mouse@126.com>
2016-01-19 22:48:13 +0800

    Updates.

---
+++ MCF/Hash/Crc64.cpp
@@ -0,0 +1,90 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2016, LH_Mouse. All wrongs reserved.
+
+#include "../StdMCF.hpp"
+#include "Crc64.hpp"
+#include "../Core/Array.hpp"
+#include "../Utilities/Endian.hpp"
+
+// http://www.relisoft.com/science/CrcOptim.html
+// 1. 原文提供的是正序（权较大位向权较小位方向）的 Crc 计算，而这里使用的是反序（权较小位向权较大位方向）。
+// 2. 原文的 CRC 余数的初始值是 0；此处以 -1 为初始值，计算完成后进行按位反。
+
+// 按照 ECMA-182 描述的算法，除数为 0xC96C5795D7870F42。
+
+namespace MCF {
+
+namespace {
+	template<unsigned kRoundT, std::uint64_t kRegT>
+	struct ElementGenerator {
+		static constexpr std::uint64_t kValue = ElementGenerator<kRoundT + 1, (kRegT >> 1) ^ ((kRegT & 1) ? 0xC96C5795D7870F42 : 0)>::kValue;
+	};
+	template<std::uint64_t kRegT>
+	struct ElementGenerator<8, kRegT> {
+		static constexpr std::uint64_t kValue = kRegT;
+	};
+
+	template<std::size_t ...kIndices>
+	constexpr Array<std::uint64_t, sizeof...(kIndices)> GenerateTable(std::index_sequence<kIndices...>) noexcept {
+		return { ElementGenerator<0, kIndices>::kValue... };
+	}
+
+	constexpr auto kCrcTable = GenerateTable(std::make_index_sequence<256>());
+}
+
+// 构造函数和析构函数。
+Crc64::Crc64() noexcept
+	: x_bInited(false)
+{
+}
+
+// 其他非静态成员函数。
+void Crc64::Abort() noexcept {
+	x_bInited = false;
+}
+void Crc64::Update(const void *pData, std::size_t uSize) noexcept {
+	if(!x_bInited){
+		x_u64Reg = (std::uint64_t)-1;
+
+		x_bInited = true;
+	}
+
+	const auto DoCrc64Byte = [&](unsigned char byData){
+		x_u64Reg = kCrcTable[(x_u64Reg ^ byData) & 0xFF] ^ (x_u64Reg >> 8);
+	};
+
+	register auto pbyRead = static_cast<const unsigned char *>(pData);
+	const auto pbyEnd = pbyRead + uSize;
+
+	if(uSize >= sizeof(std::uintptr_t) * 2){
+		while(((std::uintptr_t)pbyRead & (sizeof(std::uintptr_t) - 1)) != 0){
+			DoCrc64Byte(*pbyRead);
+			++pbyRead;
+		}
+		register auto i = (std::size_t)(pbyEnd - pbyRead) / sizeof(std::uintptr_t);
+		while(i != 0){
+			register auto uWord = LoadLe(*(const std::uintptr_t *)pbyRead);
+			pbyRead += sizeof(std::uintptr_t);
+			for(unsigned j = 0; j < sizeof(std::uintptr_t); ++j){
+				DoCrc64Byte(uWord & 0xFF);
+				uWord >>= 8;
+			}
+			--i;
+		}
+	}
+	while(pbyRead != pbyEnd){
+		DoCrc64Byte(*pbyRead);
+		++pbyRead;
+	}
+}
+std::uint64_t Crc64::Finalize() noexcept {
+	if(x_bInited){
+		x_u64Reg = ~x_u64Reg;
+
+		x_bInited = false;
+	}
+	return x_u64Reg;
+}
+
+}
--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,16 +1,25 @@
 #include <MCF/StdMCF.hpp>
 #include <MCF/Hash/Crc32.hpp>
+#include <MCF/Hash/Crc64.hpp>
 #include <MCF/Core/StringView.hpp>
 #include <cstdio>
 
 using namespace MCF;
 
 extern "C" unsigned MCFCRT_Main(){
-	Crc32 crc;
 	const auto str = "hello world!"_nsv;
+
+	Crc32 crc32;
+	crc32.Update(str.GetBegin(), str.GetSize());
+	unsigned long long val = crc32.Finalize();
+	// crc32 = 0x03B4C26D
+	std::printf("crc32 = 0x%08llX\n", val);
+
+	Crc64 crc;
 	crc.Update(str.GetBegin(), str.GetSize());
-	auto val = crc.Finalize();
-	// crc = 0x03B4C26D
-	std::printf("crc = 0x%08lX\n", (unsigned long)val);
+	val = crc.Finalize();
+	// crc64 = 0x8483C0FA32607D61
+	std::printf("crc64 = 0x%016llX\n", val);
+
 	return 0;
 }

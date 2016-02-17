6edb808b0d464084f76de9a91fb6437a25106549
lhmouse <lh_mouse@126.com>
2016-02-17 22:55:44 +0800

    Added BlackHole.

---
+++ MCF/Function/BlackHole.hpp
@@ -0,0 +1,18 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2016, LH_Mouse. All wrongs reserved.
+
+#ifndef MCF_FUNCTION_BLACK_HOLE_HPP_
+#define MCF_FUNCTION_BLACK_HOLE_HPP_
+
+namespace MCF {
+
+struct BlackHole {
+	template<typename ...ParamsT>
+	void operator()(ParamsT &&...) const noexcept {
+	}
+};
+
+}
+
+#endif

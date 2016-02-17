b72e33ca9bbf2e6a99cc841c9999718c0c072a4f
lhmouse <lh_mouse@126.com>
2016-02-17 18:24:11 +0800

    Updates.

--- MCF/Utilities/AlignedStorage.hpp
+++ MCF/Utilities/AlignedStorage.hpp
@@ -2,14 +2,14 @@
 // 有关具体授权说明，请参阅 MCFLicense.txt。
 // Copyleft 2013 - 2016, LH_Mouse. All wrongs reserved.
 
-#ifndef MCF_UTILITIES_ALIGNED_UNION_HPP_
-#define MCF_UTILITIES_ALIGNED_UNION_HPP_
+#ifndef MCF_UTILITIES_ALIGNED_STORAGE_HPP_
+#define MCF_UTILITIES_ALIGNED_STORAGE_HPP_
 
 #include <cstddef>
 
 namespace MCF {
 
-namespace Impl_AlignedUnion {
+namespace Impl_AlignedStorage {
 	template<std::size_t kFirstT, std::size_t kSecondT>
 	struct MaxHelperBinary {
 		static constexpr std::size_t kValue = (kFirstT > kSecondT) ? kFirstT : kSecondT;
@@ -26,8 +26,8 @@ namespace Impl_AlignedUnion {
 }
 
 template<std::size_t kMinSizeT, typename ...ElementsT>
-union AlignedUnion {
-	alignas(Impl_AlignedUnion::MaxHelper<alignof(ElementsT)...>::kValue) char a[Impl_AlignedUnion::MaxHelper<kMinSizeT, sizeof(ElementsT)...>::kValue];
+struct AlignedStorage {
+	alignas(Impl_AlignedStorage::MaxHelper<alignof(ElementsT)...>::kValue) char a[Impl_AlignedStorage::MaxHelper<kMinSizeT, sizeof(ElementsT)...>::kValue];
 };
 
 }

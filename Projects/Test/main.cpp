5a3bfa8426d6739ac7706f79b7aa079b1814cb37
lhmouse <lh_mouse@126.com>
2016-03-08 16:30:35 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -7,18 +7,32 @@
 using namespace MCF;
 
 extern "C" unsigned MCFCRT_Main(){
-	constexpr unsigned char str[] = "hello world!";
-	for(unsigned len = 0; len < sizeof(str); ++len){
-		auto os = MakeIntrusive<BufferOutputStream>();
-		auto bs = MakeIntrusive<Base64OutputStreamFilter>(os);
-		bs->Put(str, len);
-		bs->Flush(false);
+	constexpr char strs[][64] = {
+		"aA==",
+		"aGU=",
+		"aGVs",
+		"aGVsbA==",
+		"aGVsbG8=",
+		"aGVsbG8g",
+		"aGVsbG8gdw==",
+		"aGVsbG8gd28=",
+		"aGVsbG8gd29y",
+		"aGVsbG8gd29ybA==",
+		"aGVsbG8gd29ybGQ=",
+		"aGVsbG8gd29ybGQh",
+	};
+
+	for(unsigned i = 0; i < sizeof(strs) / sizeof(strs[0]); ++i){
+		auto is = MakeIntrusive<BufferInputStream>();
+		is.GetBuffer().Put(strs[i], std::strlen(strs[i]));
+		auto bs = MakeIntrusive<Base64InputStreamFilter>(is);
 
 		int c;
-		while((c = os->GetBuffer().Get()) >= 0){
+		while((c = bs->Get()) >= 0){
 			std::putchar(c);
 		}
 		std::putchar('\n');
 	}
+
 	return 0;
 }

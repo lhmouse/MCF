eb3da4ae9c3997045ba17e74cd9e0115852b2650
lhmouse <lh_mouse@126.com>
2016-03-09 15:15:36 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -5,7 +5,7 @@ volatile auto pf1 = (double (*)(double))&std::cos;
 volatile auto pf2 = (double (*)(double))::GetProcAddress(::LoadLibraryA("msvcrt.dll"), "cos");
 
 extern "C" unsigned MCFCRT_Main(){
-	const auto r = 0x1p100;
+	const auto r = 0x1p110;
 
 	std::printf("val = %f, msvc = %f, const = %f\n", (*pf1)(r), (*pf2)(r), __builtin_cos(r));
 

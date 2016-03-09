6fd84f5bfb6a79ea45139625089e10913acd70bc
lhmouse <lh_mouse@126.com>
2016-03-09 16:59:04 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -4,7 +4,7 @@
 using namespace MCF;
 
 extern "C" unsigned MCFCRT_Main(){
-	auto a = (std::puts("hello") == 0) ? 1 : DEBUG_THROW(Exception, ERROR_INVALID_PARAMETER, ""_rcs);
+	auto a = (std::puts("hello") >= 0) ? 1 : DEBUG_THROW(Exception, ERROR_INVALID_PARAMETER, ""_rcs);
 	std::printf("a = %d\n", a);
 	return 0;
 }

a8978a97b2c739921927645806a494c484885975
lhmouse <lh_mouse@126.com>
2016-03-15 13:37:25 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,11 +1,19 @@
 #include <MCF/StdMCF.hpp>
-#include <MCF/Streams/Fnv1a32OutputStream.hpp>
+#include <MCF/Core/String.hpp>
+#include <MCF/Streams/Md5OutputStream.hpp>
 
 extern "C" unsigned MCFCRT_Main(){
 	constexpr char str[] = "123 is a baka!";
-	MCF::Fnv1a32OutputStream s;
+	MCF::Md5OutputStream s;
 	s.Put(str, sizeof(str) - 1);
-	unsigned long val = s.Finalize();
-	std::printf("str = %s, fnv1a(str) = %08lx\n", str, val);
+	const auto val = s.Finalize();
+
+	MCF::NarrowString ln;
+	for(auto b : val){
+		char temp[8];
+		int tlen = std::sprintf(temp, "%02hhx", b);
+		ln.Append(temp, (unsigned)tlen);
+	}
+	std::printf("str = %s, md5 = %s\n", str, ln.GetStr());
 	return 0;
 }
--- Projects/Test/make32.cmd
+++ Projects/Test/make32.cmd
@@ -7,14 +7,14 @@ if "%1"=="Release" (
 	set CXXFlags=-DNDEBUG -O3 -ffunction-sections -fdata-sections -Wl,--gc-sections,-s
 )
 
-set Lib=-lmcf -lz -llzmalite -lmcfcrt -lstdc++ -lmingwex -lmingw32 -lgcc -lgcc_eh -lmcfcrt -lgcc -lmingwex -lmsvcrt -lkernel32 -lntdll -luser32 -lshell32 -ladvapi32
+set Lib=-lmcf -lmcfcrt -lstdc++ -lmingwex -lmingw32 -lgcc -lgcc_eh -lmcfcrt -lgcc -lmingwex -lmsvcrt -lkernel32 -lntdll -luser32 -lshell32 -ladvapi32
 
 call mingw 32
 
 mcfbuild -p../../MCFCRT/MCFBuild.mcfproj -s../../MCFCRT -d../../.Built/%Config%/mcfcrt -o../../.Built/%Config%/libmcfcrt.a %* || exit /b 1
 mcfbuild -p../../MCF/MCFBuild.mcfproj -s../../MCF -d../../.Built/%Config%/mcf -o../../.Built/%Config%/libmcf.a %* || exit /b 1
-mcfbuild -p../../External/dlmalloc/MCFBuild.mcfproj -s../../External/dlmalloc -d../../.Built/%Config%/dlmalloc -o../../.Built/%Config%/libdlmalloc.a %* || exit /b 1
-mcfbuild -p../../External/zlib/MCFBuild.mcfproj -s../../External/zlib -d../../.Built/%Config%/zlib -o../../.Built/%Config%/libz.a %* || exit /b 1
-mcfbuild -p../../External/lzmalite/MCFBuild.mcfproj -s../../External/lzmalite -d../../.Built/%Config%/lzmalite -o../../.Built/%Config%/liblzmalite.a %* || exit /b 1
+:: mcfbuild -p../../External/dlmalloc/MCFBuild.mcfproj -s../../External/dlmalloc -d../../.Built/%Config%/dlmalloc -o../../.Built/%Config%/libdlmalloc.a %* || exit /b 1
+:: mcfbuild -p../../External/zlib/MCFBuild.mcfproj -s../../External/zlib -d../../.Built/%Config%/zlib -o../../.Built/%Config%/libz.a %* || exit /b 1
+:: mcfbuild -p../../External/lzmalite/MCFBuild.mcfproj -s../../External/lzmalite -d../../.Built/%Config%/lzmalite -o../../.Built/%Config%/liblzmalite.a %* || exit /b 1
 
 g++ %CXXFlags% -std=c++14 -Wnoexcept -fno-enforce-eh-specs -fnothrow-opt -Wall -Wextra -pedantic -pedantic-errors -Wsign-conversion -Wsuggest-attribute=noreturn -pipe -mfpmath=both -march=nocona -mno-stack-arg-probe -mno-accumulate-outgoing-args -mpush-args -masm=intel main.cpp -o ".%Config%.exe" -I../.. -L../../.Built/%Config% -static -nostdlib -Wl,-e__MCFCRT_ExeStartup,--disable-runtime-pseudo-reloc,--disable-auto-import,--wrap=atexit,--wrap=abort,--wrap=malloc,--wrap=realloc,--wrap=calloc,--wrap=free,--wrap=__cxa_throw %Lib% || exit /b 1

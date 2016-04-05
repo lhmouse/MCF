31b00ab198725fb994822e323decacb5307404cc
lhmouse <lh_mouse@126.com>
2016-04-05 10:15:25 +0800

    .

--- MCFCRT/env/eh_top.c
+++ MCFCRT/env/eh_top.c
@@ -30,6 +30,8 @@ const volatile __auto_type __MCFCRT_deregister_frame_info = &__deregister_frame_
 
 __extension__ __attribute__((__section__(".eh_fram$@@@"), __used__))
 static const char eh_begin[0] = { };
+__extension__ __attribute__((__section__(".eh_fram$___"), __used__))
+static const char eh_end[16]  = { };
 
 static struct object eh_obj;
 static void *eh_frame_base;

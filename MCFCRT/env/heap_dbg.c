a00803d36bfd536be52f0d3ef4214840354bd154
lhmouse <lh_mouse@126.com>
2016-04-05 18:29:45 +0800

    .

--- MCFCRT/env/heap_dbg.c
+++ MCFCRT/env/heap_dbg.c
@@ -14,9 +14,6 @@
 
 #if __MCFCRT_REQUIRE_HEAPDBG_LEVEL(3)
 
-static HANDLE          g_hMapAllocator = nullptr;
-static _MCFCRT_AvlRoot g_avlBlocks     = nullptr;
-
 static int BlockInfoComparatorNodeKey(const _MCFCRT_AvlNodeHeader *pInfo1, intptr_t nKey2){
 	const uintptr_t uKey1 = (uintptr_t)(((const __MCFCRT_HeapDbgBlockInfo *)pInfo1)->__pContents);
 	const uintptr_t uKey2 = (uintptr_t)(void *)nKey2;
@@ -25,6 +22,48 @@ static int BlockInfoComparatorNodeKey(const _MCFCRT_AvlNodeHeader *pInfo1, intpt
 static int BlockInfoComparatorNodes(const _MCFCRT_AvlNodeHeader *pInfo1, const _MCFCRT_AvlNodeHeader *pInfo2){
 	return BlockInfoComparatorNodeKey(pInfo1, (intptr_t)(((const __MCFCRT_HeapDbgBlockInfo *)pInfo2)->__pContents));
 }
+
+static void DumpMemoryLeaks(_MCFCRT_AvlRoot avlBlocks){
+	if(!avlBlocks){
+		return;
+	}
+
+	const HANDLE hStdErr = GetStdHandle(STD_ERROR_HANDLE);
+	if((hStdErr == INVALID_HANDLE_VALUE) || (hStdErr == nullptr)){
+		_MCFCRT_Bail(L"__MCFCRT_HeapDbgUninit() 失败：侦测到内存泄漏。无法打开标准错误流，没有生成内存泄漏信息。");
+	}
+
+	for(const __MCFCRT_HeapDbgBlockInfo *pInfo = (const __MCFCRT_HeapDbgBlockInfo *)_MCFCRT_AvlFront(&avlBlocks);
+		pInfo; pInfo = (const __MCFCRT_HeapDbgBlockInfo *)_MCFCRT_AvlNext((const _MCFCRT_AvlNodeHeader *)pInfo))
+	{
+		char achTemp[1024];
+		char *pchWrite = achTemp;
+		pchWrite = _MCFCRT_stpcpy(pchWrite, "Memory leak: address = ");
+		pchWrite = _MCFCRT_itoa0X(pchWrite, (uintptr_t)pInfo->__pContents, sizeof(pInfo->__pContents) * 2);
+		pchWrite = _MCFCRT_stpcpy(pchWrite, ", size = ");
+		pchWrite = _MCFCRT_itoa0X(pchWrite, (uintptr_t)pInfo->__uSize, sizeof(pInfo->__uSize) * 2);
+		pchWrite = _MCFCRT_stpcpy(pchWrite, ", return address = ");
+		pchWrite = _MCFCRT_itoa0X(pchWrite, (uintptr_t)pInfo->__pRetAddr, sizeof(pInfo->__pRetAddr) * 2);
+		pchWrite = _MCFCRT_stpcpy(pchWrite, ", leading bytes =");
+		for(size_t i = 0; i < 16; ++i){
+			const unsigned char *const p = (const unsigned char *)pInfo->__pContents + i;
+			*(pchWrite++) = ' ';
+			if(IsBadReadPtr(p, 1)){
+				pchWrite = _MCFCRT_stpcpy(pchWrite, "??");
+			} else {
+				pchWrite = _MCFCRT_itoa0X(pchWrite, *p, 2);
+			}
+		}
+		*(pchWrite++) = '\n';
+
+		DWORD dwBytesWritten;
+		WriteFile(hStdErr, achTemp, (DWORD)(pchWrite - achTemp), &dwBytesWritten, nullptr);
+	}
+	_MCFCRT_Bail(L"__MCFCRT_HeapDbgUninit() 失败：侦测到内存泄漏。内存泄漏的详细信息已经输出至标准错误流中。");
+}
+
+static HANDLE          g_hMapAllocator = nullptr;
+static _MCFCRT_AvlRoot g_avlBlocks     = nullptr;
 
 #endif
 
@@ -51,45 +90,7 @@ void __MCFCRT_HeapDbgUninit(){
 	const _MCFCRT_AvlRoot avlBlocks = g_avlBlocks;
 	g_avlBlocks = nullptr;
 
-	if(avlBlocks){
-		const HANDLE hStdErr = GetStdHandle(STD_ERROR_HANDLE);
-		if(hStdErr == INVALID_HANDLE_VALUE){
-			_MCFCRT_Bail(L"__MCFCRT_HeapDbgUninit() 失败：侦测到内存泄漏。无法打开标准错误流，没有生成内存泄漏信息。");
-		}
-		if(hStdErr){
-			const __MCFCRT_HeapDbgBlockInfo *pBlockInfo = (const __MCFCRT_HeapDbgBlockInfo *)_MCFCRT_AvlFront(&avlBlocks);
-			do {
-				const unsigned char *pbyDump = pBlockInfo->__pContents;
-
-				char achTemp[1024];
-				char *pchWrite = achTemp;
-				pchWrite = _MCFCRT_stpcpy(pchWrite, "Memory leak: address = ");
-				pchWrite = _MCFCRT_itoa0X(pchWrite, (uintptr_t)pbyDump, sizeof(pbyDump) * 2);
-				pchWrite = _MCFCRT_stpcpy(pchWrite, ", size = ");
-				pchWrite = _MCFCRT_itoa0X(pchWrite, (uintptr_t)pBlockInfo->__uSize, sizeof(pBlockInfo->__uSize) * 2);
-				pchWrite = _MCFCRT_stpcpy(pchWrite, ", return address = ");
-				pchWrite = _MCFCRT_itoa0X(pchWrite, (uintptr_t)pBlockInfo->__pRetAddr, sizeof(pBlockInfo->__pRetAddr) * 2);
-				pchWrite = _MCFCRT_stpcpy(pchWrite, ", leading bytes =");
-				for(size_t i = 0; i < 16; ++i){
-					*(pchWrite++) = ' ';
-					if(IsBadReadPtr(pbyDump, 1)){
-						pchWrite = _MCFCRT_stpcpy(pchWrite, "??");
-					} else {
-						pchWrite = _MCFCRT_itoa0X(pchWrite, *pbyDump, 2);
-					}
-					++pbyDump;
-				}
-				*(pchWrite++) = '\n';
-
-				DWORD dwBytesWritten;
-				WriteFile(hStdErr, achTemp, (DWORD)(pchWrite - achTemp), &dwBytesWritten, nullptr);
-
-				pBlockInfo = (const __MCFCRT_HeapDbgBlockInfo *)_MCFCRT_AvlNext((const _MCFCRT_AvlNodeHeader *)pBlockInfo);
-			} while(pBlockInfo);
-		}
-
-		_MCFCRT_Bail(L"__MCFCRT_HeapDbgUninit() 失败：侦测到内存泄漏。内存泄漏的详细信息已经输出至标准错误流中。");
-	}
+	DumpMemoryLeaks(avlBlocks);
 	HeapDestroy(hAllocator);
 #endif
 }
@@ -105,18 +106,18 @@ size_t __MCFCRT_HeapDbgGetRawSize(size_t uContentSize){
 __MCFCRT_HeapDbgBlockInfo *__MCFCRT_HeapDbgAllocateBlockInfo(){
 	return HeapAlloc(g_hMapAllocator, 0, sizeof(__MCFCRT_HeapDbgBlockInfo));
 }
-void __MCFCRT_HeapDbgDeallocateBlockInfo(__MCFCRT_HeapDbgBlockInfo *pBlockInfo){
-	HeapFree(g_hMapAllocator, 0, pBlockInfo);
+void __MCFCRT_HeapDbgDeallocateBlockInfo(__MCFCRT_HeapDbgBlockInfo *pInfo){
+	HeapFree(g_hMapAllocator, 0, pInfo);
 }
 
-unsigned char *__MCFCRT_HeapDbgRegisterBlockInfo(__MCFCRT_HeapDbgBlockInfo *pBlockInfo, unsigned char *pRaw, _MCFCRT_STD size_t uContentSize, const void *pRetAddr){
+unsigned char *__MCFCRT_HeapDbgRegisterBlockInfo(__MCFCRT_HeapDbgBlockInfo *pInfo, unsigned char *pRaw, _MCFCRT_STD size_t uContentSize, const void *pRetAddr){
 	unsigned char *const pContents = pRaw + GUARD_BAND_SIZE;
 	const size_t uSize = uContentSize;
 
-	pBlockInfo->__pContents = pContents;
-	pBlockInfo->__uSize     = uContentSize;
-	pBlockInfo->__pRetAddr  = pRetAddr;
-	_MCFCRT_AvlAttach(&g_avlBlocks, (_MCFCRT_AvlNodeHeader *)pBlockInfo, &BlockInfoComparatorNodes);
+	pInfo->__pContents = pContents;
+	pInfo->__uSize     = uContentSize;
+	pInfo->__pRetAddr  = pRetAddr;
+	_MCFCRT_AvlAttach(&g_avlBlocks, (_MCFCRT_AvlNodeHeader *)pInfo, &BlockInfoComparatorNodes);
 
 	void **ppGuard1 = (void **)pContents;
 	void **ppGuard2 = (void **)(pContents + uSize);
@@ -136,8 +137,8 @@ __MCFCRT_HeapDbgBlockInfo *__MCFCRT_HeapDbgValidateBlock(unsigned char **ppRaw, 
 	unsigned char *const pRaw = pContents - GUARD_BAND_SIZE;
 	*ppRaw = pRaw;
 
-	__MCFCRT_HeapDbgBlockInfo *const pBlockInfo = (__MCFCRT_HeapDbgBlockInfo *)_MCFCRT_AvlFind(&g_avlBlocks, (intptr_t)pContents, &BlockInfoComparatorNodeKey);
-	if(!pBlockInfo){
+	__MCFCRT_HeapDbgBlockInfo *const pInfo = (__MCFCRT_HeapDbgBlockInfo *)_MCFCRT_AvlFind(&g_avlBlocks, (intptr_t)pContents, &BlockInfoComparatorNodeKey);
+	if(!pInfo){
 		wchar_t awchTemp[256];
 		wchar_t *pwcWrite = awchTemp;
 		pwcWrite = _MCFCRT_wcpcpy(pwcWrite, L"__MCFCRT_HeapDbgValidate() 失败：传入的指针无效。\n返回地址：");
@@ -145,7 +146,7 @@ __MCFCRT_HeapDbgBlockInfo *__MCFCRT_HeapDbgValidateBlock(unsigned char **ppRaw, 
 		*pwcWrite = 0;
 		_MCFCRT_Bail(awchTemp);
 	}
-	const size_t uSize = pBlockInfo->__uSize;
+	const size_t uSize = pInfo->__uSize;
 
 	void *const *ppGuard1 = (void *const *)pContents;
 	void *const *ppGuard2 = (void *const *)(pContents + uSize);
@@ -167,10 +168,10 @@ __MCFCRT_HeapDbgBlockInfo *__MCFCRT_HeapDbgValidateBlock(unsigned char **ppRaw, 
 		++ppGuard2;
 	}
 
-	return pBlockInfo;
+	return pInfo;
 }
-void __MCFCRT_HeapDbgUnregisterBlockInfo(__MCFCRT_HeapDbgBlockInfo *pBlockInfo){
-	_MCFCRT_AvlDetach((const _MCFCRT_AvlNodeHeader *)pBlockInfo);
+void __MCFCRT_HeapDbgUnregisterBlockInfo(__MCFCRT_HeapDbgBlockInfo *pInfo){
+	_MCFCRT_AvlDetach((const _MCFCRT_AvlNodeHeader *)pInfo);
 }
 
 #	else

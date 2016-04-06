f1b5aa62183d877f3661781ae85246abd6449637
lhmouse <lh_mouse@126.com>
2016-04-06 17:44:34 +0800

    .

--- MCFCRT/env/condition_variable.c
+++ MCFCRT/env/condition_variable.c
@@ -45,22 +45,20 @@ static inline bool ReallyWaitForConditionVariable(volatile uintptr_t *puControl,
 				} while(_MCFCRT_EXPECT_NOT(!__atomic_compare_exchange_n(puControl, &uOld, uNew, false, __ATOMIC_RELAXED, __ATOMIC_RELAXED)));
 			}
 			if(bDecremented){
-				(*pfnRelockCallback)(nContext, nLocked);
-				return false;
+				return (*pfnRelockCallback)(nContext, nLocked), false;
 			}
 			lStatus = NtWaitForKeyedEvent(nullptr, (void *)puControl, false, nullptr);
 			_MCFCRT_ASSERT_MSG(NT_SUCCESS(lStatus), L"NtWaitForKeyedEvent() 失败。");
 			_MCFCRT_ASSERT(lStatus != STATUS_TIMEOUT);
 			(*pfnRelockCallback)(nContext, nLocked);
-			return true;
+			return (*pfnRelockCallback)(nContext, nLocked), true;
 		}
 	} else {
 		NTSTATUS lStatus = NtWaitForKeyedEvent(nullptr, (void *)puControl, false, nullptr);
 		_MCFCRT_ASSERT_MSG(NT_SUCCESS(lStatus), L"NtWaitForKeyedEvent() 失败。");
 		_MCFCRT_ASSERT(lStatus != STATUS_TIMEOUT);
 	}
-	(*pfnRelockCallback)(nContext, nLocked);
-	return true;
+	return (*pfnRelockCallback)(nContext, nLocked), true;
 }
 static inline size_t ReallySignalConditionVariable(volatile uintptr_t *puControl, size_t uMaxCountToSignal){
 	uintptr_t uCountToSignal;

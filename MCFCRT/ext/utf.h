bd1c0047f8e07e0a4a98a7a4caa5a74e7a1665b8
lhmouse <lh_mouse@126.com>
2016-04-27 16:59:50 +0800

    .

--- MCFCRT/ext/utf.h
+++ MCFCRT/ext/utf.h
@@ -9,6 +9,10 @@
 
 _MCFCRT_EXTERN_C_BEGIN
 
+// 这些函数并不对字符串结束符进行处理（字符串结束符被当做普通字符）。
+// 这些函数以运行效率为首要目标，因此没有进行错误检测。
+// 为了防止访问越界，输入的缓冲区应当能够容纳合法的最大编码点。对于 UTF-8 而言这个值是 4 个编码单元（char），而对于 UTF-16 这个值是 2 个编码单元（char16_t）。
+
 extern char32_t _MCFCRT_DecodeUtf8 (const char     **__ppchRead ) _MCFCRT_NOEXCEPT;
 extern char32_t _MCFCRT_DecodeUtf16(const char16_t **__ppc16Read) _MCFCRT_NOEXCEPT;
 

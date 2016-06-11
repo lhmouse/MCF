1b5ae839aed0e9610474c3153032144a0ace14a5
lhmouse <lh_mouse@126.com>
2016-06-11 19:41:48 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,13 +1,14 @@
 #include <MCF/StdMCF.hpp>
-#include <MCFCRT/ext/div64.h>
 #include <cstdio>
 
-constexpr std::uint64_t a = 987654321987;
-constexpr std::uint32_t b = 65432178;
+__attribute__((__nonnull__(1), __noinline__))
+void foo(void *p){
+	(void)(((p) ? true : false) );
+	p && std::puts("always happen");
+}
 
 extern "C" unsigned _MCFCRT_Main(void) noexcept {
-	std::uint32_t quo, rem;
-	quo = _MCFCRT_udivrem64(&rem, a, b);
-	std::printf("quo = %llu, rem = %llu\n", (unsigned long long)quo, (unsigned long long)rem);
+	int a;
+	foo(&a);
 	return 0;
 }

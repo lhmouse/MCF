75dc6fd1923b41601ee1749fc7c0f769eeac5062
lhmouse <lh_mouse@126.com>
2016-06-29 23:06:18 +0800

    .

--- MCFCRT/env/_seh_top.h
+++ MCFCRT/env/_seh_top.h
@@ -19,7 +19,8 @@ _MCFCRT_EXTERN_C_END
 #define __MCFCRT_HAS_SEH_TOP         __attribute__((__section__(".text$__MCFCRT")))
 
 #ifdef _WIN64
-#	define __MCFCRT_SEH_TOP_BEGIN    __asm__ volatile (	\
+
+#define __MCFCRT_SEH_TOP_BEGIN      __asm__ volatile (	\
 	                                    "53933: \n"	\
 	                                    "	.seh_handler __C_specific_handler, @except \n"	\
 	                                    "	.seh_handlerdata \n"	\
@@ -27,14 +28,29 @@ _MCFCRT_EXTERN_C_END
 	                                    "	.rva 53933b, 53933f, @__MCFCRT_TopSehHandler, 53933f \n"	\
 	                                    "	.section .text$__MCFCRT \n"	\
                                     );
-#	define __MCFCRT_SEH_TOP_END      __asm__ volatile (	\
+#define __MCFCRT_SEH_TOP_END        __asm__ volatile (	\
 	                                    "	nop \n"	\
 	                                    "	.balign 16 \n"	\
 	                                    "53933: \n"	\
 	                                    "	.balign 1 \n"	\
                                     );
+
 #else
-	
+
+#define __MCFCRT_SEH_TOP_BEGIN      __asm__ volatile (	\
+	                                    "53933: \n"	\
+	                                    "	.seh_handler __C_specific_handler, @except \n"	\
+	                                    "	.seh_handlerdata \n"	\
+	                                    "	.long 1 \n"	\
+	                                    "	.rva 53933b, 53933f, @__MCFCRT_TopSehHandler, 53933f \n"	\
+	                                    "	.section .text$__MCFCRT \n"	\
+                                    );
+#define __MCFCRT_SEH_TOP_END        __asm__ volatile (	\
+	                                    "	nop \n"	\
+	                                    "	.balign 16 \n"	\
+	                                    "53933: \n"	\
+	                                    "	.balign 1 \n"	\
+                                    );
 
 #endif
 

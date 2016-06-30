5ed7091fe82bf6d80b7e36740ea75881411f637b
lhmouse <lh_mouse@126.com>
2016-06-30 15:04:54 +0800

    .

--- MCFCRT/env/_crtdef.h
+++ MCFCRT/env/_crtdef.h
@@ -13,39 +13,51 @@
 #	error This compiler does not support C++14.
 #endif
 
+#if defined(__cplusplus) && __cplusplus >= 201402l
+#	define _MCFCRT_CXX14                1
+#endif
+
+#if defined(__cplusplus) && __cplusplus >= 201103l
+#	define _MCFCRT_CXX11                1
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdalign.h>
+#include <assert.h>
+
 #ifdef __cplusplus
-#	define restrict                     __restrict__
-#	include <cstddef>
-#	include <cstdint>
-#	include <climits>
-#	include <cstring>
-#	include <cassert>
 #	define _MCFCRT_EXTERN_C_BEGIN       extern "C" {
 #	define _MCFCRT_EXTERN_C_END         }
-#	define _MCFCRT_STD                  ::std::
-#	define _MCFCRT_NOEXCEPT             noexcept
-#	define _MCFCRT_CONSTEXPR            constexpr
+#	define _MCFCRT_STD                  ::
 #else
-#	define nullptr                      ((void *)0)
-#	include <stdbool.h>                 // bool, true, false
-#	include <stdalign.h>                // alignof, alignas
-#	include <uchar.h>                   // char16_t, char32_t
-#	include <stddef.h>
-#	include <stdint.h>
-#	include <limits.h>
-#	include <string.h>
-#	include <assert.h>
-#	define _MCFCRT_EXTERN_C_BEGIN       //
-#	define _MCFCRT_EXTERN_C_END         //
+#	define _MCFCRT_EXTERN_C_BEGIN
+#	define _MCFCRT_EXTERN_C_END
 #	define _MCFCRT_STD                  //
-#	define _MCFCRT_NOEXCEPT             //
-#	define _MCFCRT_CONSTEXPR            static inline
+#endif
+
+#define restrict                        __restrict__
+
+#ifndef _MCFCRT_CXX11
+#	define nullptr                      0
 #endif
 
 #define __MCFCRT_C_CALLBACK_DECL        __attribute__((__nothrow__, __force_align_arg_pointer__, __aligned__(16)))
-
 #define __MCFCRT_C_CDECL                __attribute__((__cdecl__))    __MCFCRT_C_CALLBACK_DECL
 #define __MCFCRT_C_STDCALL              __attribute__((__stdcall__))  __MCFCRT_C_CALLBACK_DECL
 #define __MCFCRT_C_FASTCALL             __attribute__((__fastcall__)) __MCFCRT_C_CALLBACK_DECL
+
+#if defined(_MCFCRT_CXX11)
+#	define _MCFCRT_NOEXCEPT             noexcept
+#	define _MCFCRT_CONSTEXPR            constexpr
+#elif defined(__cplusplus)
+#	define _MCFCRT_NOEXCEPT             throw()
+#	define _MCFCRT_CONSTEXPR            static inline
+#else
+#	define _MCFCRT_NOEXCEPT             //
+#	define _MCFCRT_CONSTEXPR            static inline
+#endif
 
 #endif

ec77a225bfef6d2a27686c96dd92a573751fd801
lhmouse <lh_mouse@126.com>
2016-07-27 14:46:09 +0800

    .

--- build_x86_64_release.sh
+++ build_x86_64_release.sh
@@ -15,7 +15,7 @@ build=x86_64-w64-mingw32
   autoreconf -i)
 
 (cd "$builddir" &&
-  CPPFLAGS='-DNrelease'	\
+  CPPFLAGS='-DNDEBUG'	\
   CFLAGS='-O3 -ffunction-sections -fdata-sections'	\
   LDFLAGS='-Wl,-s,--gc-sections'	\
   ../MCFCRT/configure --build="$build" --host="$build" --prefix="$prefix" &&

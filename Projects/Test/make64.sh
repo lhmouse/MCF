2b02ba16f6519e2e3ace2fbf4edfceda82d3dca6
lhmouse <lh_mouse@126.com>
2016-08-18 12:23:02 +0800

    Test.

---
+++ Projects/Test/make64.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CPPFLAGS+=" -Wall -Wextra -pedantic -pedantic-errors -Werror -Wno-error=unused-parameter
+	-Wwrite-strings -Wconversion -Wsign-conversion -Wsuggest-attribute=noreturn -Wundef -Wshadow -Wstrict-aliasing=2 -Wstrict-overflow=5
+	-pipe -mfpmath=both -march=nocona -mno-stack-arg-probe -mno-accumulate-outgoing-args -mpush-args -masm=intel
+	-I../../debug/mingw64/include"
+CXXFLAGS+=" -std=c++14 -Wzero-as-null-pointer-constant -Wnoexcept -Woverloaded-virtual -fnothrow-opt"
+LDFLAGS+=" -static -nostdlib -L../../debug/mingw64/lib -lmcf -lsupc++ -lmingwex -lgcc -lgcc_eh -lmcfcrt-pre-exe -lmcfcrt -lmsvcrt -lkernel32 -lntdll -Wl,-e@__MCFCRT_ExeStartup"
+
+cp -fp ../../debug/mingw64/bin/*.dll ./
+
+x86_64-w64-mingw32-g++ ${CPPFLAGS} ${CXXFLAGS} main.cpp ${LDFLAGS}

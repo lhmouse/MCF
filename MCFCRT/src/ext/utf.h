7b86de48a3dd610cd52925634e9e7392788aff09
lhmouse <lh_mouse@126.com>
2016-08-24 10:46:58 +0800

    .

--- MCFCRT/src/ext/utf.h
+++ MCFCRT/src/ext/utf.h
@@ -15,53 +15,6 @@ _MCFCRT_EXTERN_C_BEGIN
 
 // 这些函数并不对字符串结束符进行特殊处理（字符串结束符被当作普通字符）。
 
-typedef enum __MCFCRT_tagUtf8CodeUnitType {
-	_MCFCRT_kUtf8Ascii        = 1,
-	_MCFCRT_kUtf8Trailing     = 2,
-	_MCFCRT_kUtf8LeadingTwo   = 3,
-	_MCFCRT_kUtf8LeadingThree = 4,
-	_MCFCRT_kUtf8LeadingFour  = 5,
-	_MCFCRT_kUtf8Reserved     = 6,
-} _MCFCRT_Utf8CodeUnitType;
-
-__attribute__((__const__))
-__MCFCRT_UTF_INLINE_OR_EXTERN _MCFCRT_Utf8CodeUnitType _MCFCRT_GetUtf8CodeUnitType(char __chCodeUnit) _MCFCRT_NOEXCEPT {
-	_MCFCRT_STD uint_fast32_t __u32Unit = (_MCFCRT_STD uint8_t)__chCodeUnit;
-	if(__u32Unit < 0x80){
-		return _MCFCRT_kUtf8Ascii;
-	} else if(__u32Unit < 0xC0){
-		return _MCFCRT_kUtf8Trailing;
-	} else if(__u32Unit < 0xE0){
-		return _MCFCRT_kUtf8LeadingTwo;
-	} else if(__u32Unit < 0xF0){
-		return _MCFCRT_kUtf8LeadingThree;
-	} else if(__u32Unit < 0xF8){
-		return _MCFCRT_kUtf8LeadingFour;
-	} else {
-		return _MCFCRT_kUtf8Reserved;
-	}
-}
-
-typedef enum __MCFCRT_tagUtf16CodeUnitType {
-	_MCFCRT_kUtf16Normal            = 1,
-	_MCFCRT_kUtf16LeadingSurrogate  = 2,
-	_MCFCRT_kUtf16TrailingSurrogate = 3,
-} _MCFCRT_Utf16CodeUnitType;
-
-__attribute__((__const__))
-__MCFCRT_UTF_INLINE_OR_EXTERN _MCFCRT_Utf16CodeUnitType _MCFCRT_GetUtf16CodeUnitType(char16_t __c16CodeUnit) _MCFCRT_NOEXCEPT {
-	_MCFCRT_STD uint_fast32_t __u32Unit = (_MCFCRT_STD uint16_t)__c16CodeUnit;
-	if(__u32Unit < 0xD800){
-		return _MCFCRT_kUtf16Normal;
-	} else if(__u32Unit < 0xDC00){
-		return _MCFCRT_kUtf16LeadingSurrogate;
-	} else if(__u32Unit < 0xE000){
-		return _MCFCRT_kUtf16TrailingSurrogate;
-	} else {
-		return _MCFCRT_kUtf16Normal;
-	}
-}
-
 #define _MCFCRT_UTF_SUCCESS(__c_)     ((_MCFCRT_STD int32_t)(__c_) >= 0)
 
 #define _MCFCRT_UTF_INVALID_INPUT     ((char32_t)-1)  // 输入字符串不是合法的 UTF 序列。
@@ -145,6 +98,14 @@ __MCFCRT_UTF_INLINE_OR_EXTERN char32_t _MCFCRT_DecodeUtf16(const char16_t **__pp
 	}
 	const _MCFCRT_STD uint_fast32_t __u32Unit = (_MCFCRT_STD uint16_t)*(__pc16Read++);
 	_MCFCRT_STD uint_fast32_t __u32CodePoint;
+#define __MCFCRT_DECODE_ONE(__bits_)	\
+	{	\
+		const _MCFCRT_STD uint_fast32_t __u32Next = (_MCFCRT_STD uint16_t)*(__pc16Read++);	\
+		if((__u32Next < 0xDC00) || (0xE000 <= __u32Next)){	\
+			__MCFCRT_HANDLE_INVALID_INPUT(__bIgnoreEncodingErrors, __u32CodePoint, __jDone)	\
+		}	\
+		__u32CodePoint |= (__u32Next & 0x3FF) << (__bits_);	\
+	}
 	if(__u32Unit < 0xD800){
 		__u32CodePoint = __u32Unit;
 	} else if(__u32Unit < 0xDC00){
@@ -152,17 +113,14 @@ __MCFCRT_UTF_INLINE_OR_EXTERN char32_t _MCFCRT_DecodeUtf16(const char16_t **__pp
 			return _MCFCRT_UTF_PARTIAL_DATA;
 		}
 		__u32CodePoint = (__u32Unit & 0x3FF) << 10;
-		const _MCFCRT_STD uint_fast32_t __u32Next = (_MCFCRT_STD uint16_t)*(__pc16Read++);
-		if((__u32Next < 0xDC00) || (0xE000 <= __u32Next)){
-			__MCFCRT_HANDLE_INVALID_INPUT(__bIgnoreEncodingErrors, __u32CodePoint, __jDone)
-		}
-		__u32CodePoint |= (__u32Next & 0x3FF);
+		__MCFCRT_DECODE_ONE(0)
 		__u32CodePoint += 0x10000;
 	} else if(__u32Unit < 0xE000){
 		__MCFCRT_HANDLE_INVALID_INPUT(__bIgnoreEncodingErrors, __u32CodePoint, __jDone)
 	} else {
 		__u32CodePoint = __u32Unit;
 	}
+#undef __MCFCRT_DECODE_ONE
 __jDone:
 	*__ppc16Read = __pc16Read;
 	return (char32_t)__u32CodePoint;

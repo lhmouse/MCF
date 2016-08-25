651c4e9d65518e58bd989652b8a2f6deefec9966
lhmouse <lh_mouse@126.com>
2016-08-26 00:32:52 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -3,7 +3,7 @@
 #include <MCF/Core/Clocks.hpp>
 
 extern "C" unsigned _MCFCRT_Main(void) noexcept {
-	constexpr char z[] = "abcdefg αβγδεζη 一二三四五六七𤭢𤭢𤭢𤭢𤭢𤭢𤭢";
+	constexpr char z[] = "abcdefg\x00αβγδεζη\x00一二三四五六七𤭢𤭢𤭢𤭢𤭢𤭢𤭢";
 	MCF::Utf8String t;
 	for(unsigned i = 0; i < 1000; ++i){
 		t.Append(z, sizeof(z));
@@ -12,16 +12,19 @@ extern "C" unsigned _MCFCRT_Main(void) noexcept {
 	MCF::Utf32String s1;
 	MCF::Utf16String s2;
 	MCF::Cesu8String s3;
+	MCF::ModifiedUtf8String s4;
 	const auto t1 = MCF::GetHiResMonoClock();
-	for(unsigned i = 0; i < 10000; ++i){
+	for(unsigned i = 0; i < 1000; ++i){
 		s1.Clear();
 		s1 = ss;
 		s2.Clear();
 		s2 = s1;
 		s3.Clear();
 		s3 = s2;
+		s4.Clear();
+		s4 = s3;
 		ss.Clear();
-		ss = s3;
+		ss = s4;
 	}
 	const auto t2 = MCF::GetHiResMonoClock();
 	std::printf("t2 - t1 = %f, d = %d\n", t2 - t1, ss.Compare(t));

29bdd750820e0374edb034522a4087b88c240910
lhmouse <lh_mouse@126.com>
2016-08-27 18:13:57 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -12,16 +12,16 @@ extern "C" unsigned _MCFCRT_Main(void) noexcept {
 	s2.Append('a', 0x100000);
 	s2.Append('c');
 
-	const auto fname = "wcscmp"_nsv;
+	const auto fname = "wcsncmp"_nsv;
 
 	const auto test = [&](WideStringView name){
 		try {
 			const DynamicLinkLibrary dll(name);
-			const auto pf = dll.RequireProcAddress<int (*)(const wchar_t *, const wchar_t *, std::size_t n)>(fname);
+			const auto pf = dll.RequireProcAddress<int (*)(const wchar_t *, const wchar_t *, std::size_t)>(fname);
 			std::intptr_t r;
 			const auto t1 = GetHiResMonoClock();
-			for(unsigned i = 0; i < 10000; ++i){
-				r = (std::intptr_t)(*pf)(s1.GetStr(), s2.GetStr(), s2.GetSize());
+			for(unsigned i = 0; i < 1000; ++i){
+				r = (std::intptr_t)(*pf)(s1.GetStr(), s2.GetStr(), s1.GetSize());
 			}
 			const auto t2 = GetHiResMonoClock();
 			std::printf("%-10s.%s : t2 - t1 = %f, r = %td\n", AnsiString(name).GetStr(), AnsiString(fname).GetStr(), t2 - t1, r);
@@ -32,6 +32,9 @@ extern "C" unsigned _MCFCRT_Main(void) noexcept {
 
 	test("NTDLL"_wsv);
 	test("MSVCRT"_wsv);
+	test("MSVCR80"_wsv);
+	test("MSVCR90"_wsv);
+	test("MSVCR100"_wsv);
 	test("MSVCR120"_wsv);
 	test("MCFCRT-9"_wsv);
 

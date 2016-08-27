3cb0648ec385d78ac9121f1c94fe08aea4f2d354
lhmouse <lh_mouse@126.com>
2016-08-27 18:08:31 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -6,29 +6,33 @@
 extern "C" unsigned _MCFCRT_Main(void) noexcept {
 	using namespace MCF;
 
-	NarrowString s1, s2;
+	WideString s1, s2;
 	s1.Append('a', 0x100000);
-	s1.Append(1);
+	s1.Append('v');
 	s2.Append('a', 0x100000);
-	s2.Append(-1);
+	s2.Append('c');
 
-	const auto fname = "strcmp"_nsv;
+	const auto fname = "wcscmp"_nsv;
 
 	const auto test = [&](WideStringView name){
-		const DynamicLinkLibrary dll(name);
-		const auto pf = dll.RequireProcAddress<int (*)(const void *, const void *)>(fname);
-		std::intptr_t r;
-		const auto t1 = GetHiResMonoClock();
-		for(unsigned i = 0; i < 10000; ++i){
-			r = (*pf)(s1.GetStr(), s2.GetStr());
+		try {
+			const DynamicLinkLibrary dll(name);
+			const auto pf = dll.RequireProcAddress<int (*)(const wchar_t *, const wchar_t *, std::size_t n)>(fname);
+			std::intptr_t r;
+			const auto t1 = GetHiResMonoClock();
+			for(unsigned i = 0; i < 10000; ++i){
+				r = (std::intptr_t)(*pf)(s1.GetStr(), s2.GetStr(), s2.GetSize());
+			}
+			const auto t2 = GetHiResMonoClock();
+			std::printf("%-10s.%s : t2 - t1 = %f, r = %td\n", AnsiString(name).GetStr(), AnsiString(fname).GetStr(), t2 - t1, r);
+		} catch(Exception &e){
+			std::printf("%-10s.%s : error %lu\n", AnsiString(name).GetStr(), AnsiString(fname).GetStr(), e.GetErrorCode());
 		}
-		const auto t2 = GetHiResMonoClock();
-		std::printf("%-10s.%s : t2 - t1 = %f, r = %td\n", AnsiString(name).GetStr(), AnsiString(fname).GetStr(), t2 - t1, r);
 	};
 
 	test("NTDLL"_wsv);
 	test("MSVCRT"_wsv);
-	test("MSVCR100"_wsv);
+	test("MSVCR120"_wsv);
 	test("MCFCRT-9"_wsv);
 
 	return 0;

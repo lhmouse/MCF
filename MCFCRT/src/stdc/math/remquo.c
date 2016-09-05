754b0914e4e976dbcfe0288f1b8e50c49cd9605a
lhmouse <lh_mouse@126.com>
2016-09-06 00:31:58 +0800

    Would placing the quotient lookup table of `remquo` into `.text` section make it run faster?

--- MCFCRT/src/stdc/math/remquo.c
+++ MCFCRT/src/stdc/math/remquo.c
@@ -5,6 +5,7 @@
 #include "../../env/_crtdef.h"
 #include "_fpu.h"
 
+__attribute__((__section__(".text")))
 static const uint8_t quo_table[256] = {
 	0, 4, 1, 5, 0, 4, 1, 5, 0, 4, 1, 5, 0, 4, 1, 5,
 	0, 4, 1, 5, 0, 4, 1, 5, 0, 4, 1, 5, 0, 4, 1, 5,

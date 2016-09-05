5a5f4be9689b13b8cc4a610ad60534c71de78a08
lhmouse <lh_mouse@126.com>
2016-09-06 00:20:59 +0800

    Removed old math stuff.

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -6,11 +6,11 @@ extern "C" unsigned _MCFCRT_Main(void) noexcept {
 	using namespace MCF;
 
 	const DynamicLinkLibrary dll(L"MCFCRT-9.DLL"_wsv);
-	const auto f = dll.RequireProcAddress<double (*)(double)>("log1p"_nsv);
+	const auto f = dll.RequireProcAddress<double (*)(double, double)>("pow"_nsv);
 
 	for(int i = -100; i <= 100; ++i){
-		const auto d = static_cast<float>(i+100) / 100 + 0.001;
-		std::printf("f(%f) = %.16f\n", (double)d, (double)f(d));
+		const auto d = static_cast<float>((i+100) / 100 + 0.001);
+		std::printf("f(%f) = %.16f\n", (double)d, (double)f(__builtin_fabsf(d), i));
 	}
 
 	return 0;

5ec15553e6e38258b8529db8fddc6b1e75045fee
lhmouse <lh_mouse@126.com>
2016-09-08 18:47:30 +0800

    Implemented `fma()` functions.

--- MCFCRT/src/stdc/math/fma.c
+++ MCFCRT/src/stdc/math/fma.c
@@ -9,8 +9,56 @@
 #undef fma
 #undef fmal
 
+static_assert(sizeof(unsigned long) * CHAR_BIT == 32, "Please fix this!");
+
+// https://en.wikipedia.org/wiki/Extended_precision#x86_Extended_Precision_Format
+typedef union x87reg_ {
+	long double f;
+	struct __attribute__((__packed__)) {
+		uint32_t flo : 32;
+		uint32_t fhi : 32;
+		uint16_t exp : 15;
+		uint16_t sgn :  1;
+	};
+} x87reg;
+
+static inline void break_down(x87reg *restrict lo, x87reg *restrict hi, long double x){
+	hi->f = x;
+	const uint32_t flo = hi->flo;
+	const uint32_t exp = hi->exp;
+	const bool     sgn = hi->sgn;
+	hi->flo = 0;
+
+	lo->flo = 0;
+	if(flo == 0){
+		lo->fhi = 0;
+		lo->exp = 0;
+	} else {
+		const uint32_t shn = (unsigned)__builtin_clzl(flo) + 32;
+		if(exp > shn){
+			lo->fhi = flo << (shn - 32);
+			lo->exp = (exp - shn) & 0x7FFF;
+		} else {
+			if(exp > 32){
+				lo->fhi = flo << (exp - 32);
+			} else {
+				lo->fhi = flo >> (32 - exp);
+			}
+			lo->exp = 0;
+		}
+	}
+	lo->sgn = sgn;
+}
+
 static inline long double fpu_fma(long double x, long double y, long double z){
-	return x * y + z;
+	x87reg xlo, xhi, ylo, yhi;
+	break_down(&xlo, &xhi, x);
+	break_down(&ylo, &yhi, y);
+	long double ret = z;
+	ret += xhi.f * yhi.f;
+	ret += xhi.f * ylo.f + xlo.f * yhi.f;
+	ret += xlo.f * ylo.f;
+	return ret;
 }
 
 float fmaf(float x, float y, float z){
--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -6,12 +6,12 @@ extern "C" unsigned _MCFCRT_Main(void) noexcept {
 	using namespace MCF;
 
 	const DynamicLinkLibrary dll(L"MCFCRT-9.DLL"_wsv);
-	const auto f = dll.RequireProcAddress<long (*)(double)>("lround"_nsv);
+	const auto my_fmal = dll.RequireProcAddress<long double (*)(long double, long double, long double)>("fmal"_nsv);
 
-	for(int i = -100; i <= 100; ++i){
-		const auto d = static_cast<float>(i / 10.0);
-		std::printf("f(%f) = %.16f\n", (double)d, (double)f(d));
-	}
+	const auto x =  1.000000002l;
+	const auto y =  3.000000004l;
+	const auto z = -3.000000010l;
+	std::printf("my_fmal(x,y,z) = %.30Le\n", my_fmal(x,y,z));
 
 	return 0;
 }

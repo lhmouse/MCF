49cd1d12fcf209790ffab1fc618f298b42529b59
lhmouse <lh_mouse@126.com>
2016-09-23 15:52:30 +0800

    Merged BlackHole.hpp and WhiteHole.hpp.

---
+++ MCF/src/Core/BlackWhiteHole.hpp
@@ -0,0 +1,36 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2016, LH_Mouse. All wrongs reserved.
+
+#ifndef MCF_CORE_BLACK_WHITE_HOLE_HPP_
+#define MCF_CORE_BLACK_WHITE_HOLE_HPP_
+
+#include "../Config.hpp"
+
+namespace MCF {
+
+struct BlackHole {
+	template<typename ...ParamsT>
+	constexpr BlackHole(ParamsT &&...) noexcept {
+		//
+	}
+	template<typename ...ParamsT>
+	void operator()(ParamsT &&...) const noexcept {
+		//
+	}
+};
+
+struct WhiteHole {
+	template<typename ParamT>
+	constexpr operator ParamT() const {
+		return { };
+	}
+	template<typename ...ParamsT>
+	void operator()(ParamsT &...vParams) const {
+		(static_cast<void>(vParams = { }), ...);
+	}
+};
+
+}
+
+#endif
--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,15 +1,12 @@
 #include <MCF/StdMCF.hpp>
-#include <MCF/Core/Defer.hpp>
+#include <MCF/Core/BlackWhiteHole.hpp>
 #include <cstdio>
 
 extern "C" unsigned _MCFCRT_Main(void) noexcept {
-	try {
-		auto dg = MCF::Defer             ([]{ std::puts("deferred!"); });
-		auto dn = MCF::DeferOnNormalExit ([]{ std::puts("deferred on normal exit!"); });
-		auto de = MCF::DeferOnException  ([]{ std::puts("deferred on exception!"); });
-		//throw 1;
-	} catch(int e){
-		std::printf("exception caught: e = %d\n", e);
-	}
+	MCF::BlackHole bh;
+	MCF::WhiteHole wh;
+	int a = wh;
+	std::printf("a = %d\n", a);
+	bh = a;
 	return 0;
 }

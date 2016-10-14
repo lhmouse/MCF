f3569903dd6ba4af8a3b0c30c1d4d347bc1263ed
LH_Mouse <lh_mouse@126.com>
2016-10-14 17:56:45 +0800

    Revert "The DWARF exception model for x86 is no longer supported. Please use SJLJ instead."
    This reverts commit 00dfc17ad2fbe5939e5cf87f05b988276f02a138.

--- MCFCRT/src/pre/module.c
+++ MCFCRT/src/pre/module.c
@@ -54,6 +54,96 @@ static void PumpAtModuleExit(void){
 	_MCFCRT_SignalMutex(&g_vAtExitMutex);
 }
 
+typedef struct tagModuleSectionInfo {
+	char achName[8];
+	size_t uRawSize;
+	void *pBase;
+	size_t uSize;
+
+	const void *pTable;
+	size_t uCount;
+	size_t uNext;
+} ModuleSectionInfo;
+
+static bool EnumerateNextModuleSection(ModuleSectionInfo *pInfo){
+	const size_t uIndex = pInfo->uNext;
+	if(uIndex >= pInfo->uCount){
+		return false;
+	}
+	pInfo->uNext = uIndex + 1;
+
+	const IMAGE_DOS_HEADER *const pImageBase = _MCFCRT_GetModuleBase();
+	const IMAGE_SECTION_HEADER *const pHeader = (const IMAGE_SECTION_HEADER *)pInfo->pTable + uIndex;
+	memcpy(pInfo->achName, pHeader->Name, 8);
+	pInfo->uRawSize = pHeader->SizeOfRawData;
+	pInfo->pBase    = (char *)pImageBase + pHeader->VirtualAddress;
+	pInfo->uSize    = pHeader->Misc.VirtualSize;
+
+	return true;
+}
+static bool EnumerateFirstModuleSection(ModuleSectionInfo *pInfo){
+	const IMAGE_DOS_HEADER *const pImageBase = _MCFCRT_GetModuleBase();
+	if(pImageBase->e_magic != IMAGE_DOS_SIGNATURE){
+		return false;
+	}
+	const IMAGE_NT_HEADERS *const pNtHeaders = (const IMAGE_NT_HEADERS *)((char *)pImageBase + pImageBase->e_lfanew);
+	if(pNtHeaders->Signature != IMAGE_NT_SIGNATURE){
+		return false;
+	}
+
+	pInfo->pTable = (const char *)&pNtHeaders->OptionalHeader + pNtHeaders->FileHeader.SizeOfOptionalHeader;
+	pInfo->uCount = pNtHeaders->FileHeader.NumberOfSections;
+	pInfo->uNext  = 0;
+
+	return EnumerateNextModuleSection(pInfo);
+}
+
+// 参见 gcc/libgcc/unwind-dw2-fde.h 里面的 old_object 的注释。
+// 6 个指针大小应该足够，我们在这里保留到 8 个。
+struct object {
+	void *impl[8];
+};
+
+__attribute__((__weak__))
+extern void __register_frame_info(const void *, struct object *);
+__attribute__((__weak__))
+extern void *__deregister_frame_info(const void *);
+
+__extension__ __attribute__((__section__(".eh_frame"), __used__))
+static const char    g_probe[0] = { };
+
+static const void *  g_eh_base;
+static struct object g_eh_object;
+
+static void RegisterFrameInfo(void){
+	if(__register_frame_info){
+		const void *base = nullptr;
+		ModuleSectionInfo section;
+		for(bool valid = EnumerateFirstModuleSection(&section); valid; valid = EnumerateNextModuleSection(&section)){
+			const char *const begin = section.pBase;
+			const char *const end   = begin + section.uSize;
+			if((begin <= g_probe) && (g_probe < end)){
+				base = begin;
+				break;
+			}
+		}
+		if(base){
+			__register_frame_info(base, &g_eh_object);
+			g_eh_base = base;
+		}
+	}
+}
+static void DeregisterFrameInfo(void){
+	if(__deregister_frame_info){
+		const void *const base = g_eh_base;
+		if(!base){
+			return;
+		}
+		g_eh_base = nullptr;
+		__deregister_frame_info(base);
+	}
+}
+
 typedef void (*StaticConstructorDestructorProc)(void);
 
 extern const StaticConstructorDestructorProc __CTOR_LIST__[];
@@ -84,12 +174,14 @@ static void CallStaticDestructors(void){
 }
 
 bool __MCFCRT_ModuleInit(void){
+	RegisterFrameInfo();
 	CallStaticConstructors();
 	return true;
 }
 void __MCFCRT_ModuleUninit(void){
 	PumpAtModuleExit();
 	CallStaticDestructors();
+	DeregisterFrameInfo();
 	__MCFCRT_libsupcxx_freeres();
 }
 

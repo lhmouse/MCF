e64ddd954f1ff498b64c58b4992f5c4d58547c5f
LH_Mouse <lh_mouse@126.com>
2016-11-21 10:59:09 +0800

    .

---
+++ MCFCRT/src/gthread.h
@@ -0,0 +1,10 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2016, LH_Mouse. All wrongs reserved.
+
+#ifndef __MCFCRT_GTHREAD_H_
+#define __MCFCRT_GTHREAD_H_
+
+#include "env/gthread.h"
+
+#endif

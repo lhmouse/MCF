6bfe2219122cac193938d3215a95e265585eb2b0
Liu Hao <noreply@github.com>
2017-03-18 17:53:18 +0800

    Test.

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -7,6 +7,7 @@ using namespace MCF;
 extern "C" unsigned _MCFCRT_Main(void) noexcept {
 	auto fp = MakeUniqueFunction<long (int)>([](int a){ return std::printf("a = %d\n", a); });
 	auto fv = FunctionView<void (int)>(fp);
+	//
 	(*fp)(12345);
 	fv(67890);
 	return 0;

34d288115d866db1f159db2950407fb5868d1bec
Liu Hao <lh_mouse@126.com>
2017-03-19 23:53:25 +0800

    .

--- MCFCRT/src/pre/at_quick_exit.c
+++ MCFCRT/src/pre/at_quick_exit.c
@@ -6,7 +6,7 @@
 #include "../env/crt_module.h"
 
 int at_quick_exit(void (*func)(void)){
-	if(!_MCFCRT_AtCrtModuleQuickExit((__attribute__((__cdecl__)) void (*)(intptr_t))func, 0)){
+	if(!_MCFCRT_AtCrtModuleQuickExit((void (*__attribute__((__cdecl__)))(intptr_t))func, 0)){
 		return -1;
 	}
 	return 0;

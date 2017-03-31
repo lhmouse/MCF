6b0b64290b00e7c0965f207a004bd23fd485a383
Liu Hao <lh_mouse@126.com>
2017-03-31 14:43:31 +0800

    .

--- MCFCRT/src/env/heap.h
+++ MCFCRT/src/env/heap.h
@@ -12,12 +12,12 @@ _MCFCRT_EXTERN_C_BEGIN
 extern bool __MCFCRT_HeapInit(void) _MCFCRT_NOEXCEPT;
 extern void __MCFCRT_HeapUninit(void) _MCFCRT_NOEXCEPT;
 
-extern void *__MCFCRT_HeapAlloc(_MCFCRT_STD size_t __uSize, bool __bFillsWithZero, const void *__pRetAddr) _MCFCRT_NOEXCEPT
-	__attribute__((__malloc__));
-extern void *__MCFCRT_HeapRealloc(void *__pBlock, _MCFCRT_STD size_t __uSize, bool __bFillsWithZero, const void *__pRetAddr) _MCFCRT_NOEXCEPT
-	__attribute__((__nonnull__(1)));
-extern void __MCFCRT_HeapFree(void *__pBlock, const void *__pRetAddr) _MCFCRT_NOEXCEPT
-	__attribute__((__nonnull__(1)));
+__attribute__((__malloc__))
+extern void *__MCFCRT_HeapAlloc(_MCFCRT_STD size_t __uSize, bool __bFillsWithZero, const void *__pRetAddr) _MCFCRT_NOEXCEPT;
+__attribute__((__nonnull__(1)))
+extern void *__MCFCRT_HeapRealloc(void *__pBlock, _MCFCRT_STD size_t __uSize, bool __bFillsWithZero, const void *__pRetAddr) _MCFCRT_NOEXCEPT;
+__attribute__((__nonnull__(1)))
+extern void __MCFCRT_HeapFree(void *__pBlock, const void *__pRetAddr) _MCFCRT_NOEXCEPT;
 
 __attribute__((__always_inline__, __malloc__))
 static inline void *_MCFCRT_malloc(_MCFCRT_STD size_t __uSize) _MCFCRT_NOEXCEPT {

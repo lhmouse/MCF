9043a21084cbfa44984d764f5902663c2d09d2e8
Liu Hao <lh_mouse@126.com>
2017-05-20 17:18:09 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -1,49 +1,46 @@
 #include <mcfgthread/gthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <cstdio>
+#include <cstdlib>
 
-__gthread_key_t key;
+::__gthread_key_t key;
 
 void tls_destructor(void *p){
-	_MCFCRT_ASSERT(__gthread_getspecific(key) == _MCFCRT_NULLPTR);
-
-	printf("destructing tls data %u\n", *(unsigned *)p);
-	free(p);
+	_MCFCRT_ASSERT(::__gthread_getspecific(key) == nullptr);
+	std::printf("destructing tls data %u\n", *(unsigned *)p);
+	delete (unsigned *)p;
 }
 
 void *test_thread_proc(void *param){
-	unsigned *p = (unsigned *)malloc(sizeof(unsigned));
-	_MCFCRT_ASSERT(p);
-	*p = (unsigned)(uintptr_t)param;
-	int err = __gthread_setspecific(key, p);
+	const auto p = new unsigned((unsigned)(uintptr_t)param);
+	int err = ::__gthread_setspecific(key, p);
 	_MCFCRT_ASSERT(err == 0);
-	printf("constructed tls data %u\n", *(unsigned *)p);
+	std::printf("constructed tls data %u\n", *(unsigned *)p);
 
-	err = __gthread_setspecific(key, p);
+	err = ::__gthread_setspecific(key, p);
 	_MCFCRT_ASSERT(err == 0);
-	printf("set new tls data %u\n", *(unsigned *)p);
+	std::printf("set new tls data %u\n", *(unsigned *)p);
 
-	return _MCFCRT_NULLPTR;
+	return nullptr;
 }
 
 extern "C" unsigned _MCFCRT_Main(void) noexcept {
-	int err = __gthread_key_create(&key, &tls_destructor);
+	int err = ::__gthread_key_create(&key, &tls_destructor);
 	_MCFCRT_ASSERT(err == 0);
-	printf("key = %p\n", (void *)key);
+	std::printf("key = %p\n", (void *)key);
+	err = std::atexit([]{ ::__gthread_key_delete(key); });
+	_MCFCRT_ASSERT(err == 0);
 
-#define THREAD_COUNT 10u
-	__gthread_t threads[THREAD_COUNT];
-	for(unsigned i = 0; i < THREAD_COUNT; ++i){
-		err = __gthread_create(&threads[i], &test_thread_proc, (void *)(intptr_t)i);
+	static constexpr unsigned n = 10;
+	__gthread_t threads[n];
+	for(unsigned i = 0; i < n; ++i){
+		err = ::__gthread_create(&threads[i], &test_thread_proc, (void *)(intptr_t)i);
 		_MCFCRT_ASSERT(err == 0);
 	}
-	for(unsigned i = 0; i < THREAD_COUNT; ++i){
-		printf("waiting for thread %u\n", i);
-		err = __gthread_join(threads[i], _MCFCRT_NULLPTR);
+	for(unsigned i = 0; i < n; ++i){
+		std::printf("waiting for thread %u\n", i);
+		err = ::__gthread_join(threads[i], nullptr);
 		_MCFCRT_ASSERT(err == 0);
 	}
 
-	__gthread_key_delete(key);
 	return 0;
 }

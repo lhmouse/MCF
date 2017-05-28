407be8a18f9b4347eec716901773241037dd7c73
Liu Hao <lh_mouse@126.com>
2017-05-28 10:36:49 +0800

    .

--- MCFCRT/src/pre/__cxa_atexit.c
+++ MCFCRT/src/pre/__cxa_atexit.c
@@ -2,7 +2,6 @@
 // 有关具体授权说明，请参阅 MCFLicense.txt。
 // Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
 
-#include "../env/_crtdef.h"
 #include "module.h"
 
 int __cxa_atexit(void (*func)(void *), void *param, void *dso_handle){
--- MCFCRT/src/pre/__cxa_thread_atexit.c
+++ MCFCRT/src/pre/__cxa_thread_atexit.c
@@ -2,7 +2,6 @@
 // 有关具体授权说明，请参阅 MCFLicense.txt。
 // Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
 
-#include "../env/_crtdef.h"
 #include "tls.h"
 
 int __cxa_thread_atexit(void (*func)(void *), void *param, void *dso_handle){

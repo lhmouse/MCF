e6ee609545d0928453021a01c2e9415c655868ef
Liu Hao <lh_mouse@126.com>
2017-06-03 11:16:18 +0800

    .

--- MCFCRT/src/env/_seh_top.h
+++ MCFCRT/src/env/_seh_top.h
@@ -14,23 +14,23 @@ __MCFCRT_C_CDECL
 extern EXCEPTION_DISPOSITION __MCFCRT_SehTopDispatcher(EXCEPTION_RECORD *__pRecord, void *__pEstablisherFrame, CONTEXT *__pContext, void *__pDispatcherContext) _MCFCRT_NOEXCEPT;
 
 __attribute__((__unused__, __always_inline__))
-static inline void __MCFCRT_SehTopInstaller_x86(void *__seh_node) _MCFCRT_NOEXCEPT {
-	void *__seh_unused;
+static inline void __MCFCRT_SehTopInstaller_x86(void *__pSehNode) _MCFCRT_NOEXCEPT {
+	void *__pUnused;
 	__asm__ volatile (
 		"mov %0, dword ptr fs:[0] \n"
 		"mov dword ptr[%1], %0 \n"
 		"mov dword ptr[%1 + 4], offset ___MCFCRT_SehTopDispatcher \n"
 		"mov dword ptr fs:[0], %1 \n"
-		: "=&r"(__seh_unused) : "r"(__seh_node)
+		: "=&r"(__pUnused) : "r"(__pSehNode)
 	);
 }
 __attribute__((__unused__, __always_inline__))
-static inline void __MCFCRT_SehTopUninstaller_x86(void *__seh_node) _MCFCRT_NOEXCEPT {
-	void *__seh_unused;
+static inline void __MCFCRT_SehTopUninstaller_x86(void *__pSehNode) _MCFCRT_NOEXCEPT {
+	void *__pUnused;
 	__asm__ volatile (
 		"mov %0, dword ptr[%1] \n"
 		"mov dword ptr fs:[0], %0 \n"
-		: "=&r"(__seh_unused) : "r"(__seh_node)
+		: "=&r"(__pUnused) : "r"(__pSehNode)
 	);
 }
 
@@ -38,17 +38,17 @@ _MCFCRT_EXTERN_C_END
 
 #ifdef _WIN64
 #  define __MCFCRT_SEH_TOP_BEGIN        { __asm__ volatile (	\
-	                                        ".seh_handler __MCFCRT_SehTopDispatcher, @except \n");	\
-	                                      {
+                                            ".seh_handler __MCFCRT_SehTopDispatcher, @except \n");	\
+                                            {
 #  define __MCFCRT_SEH_TOP_END            }	\
-	                                    }
+                                        }
 #else
-#  define __MCFCRT_SEH_TOP_BEGIN        { void *__MCFCRT_seh_node[2]	\
-	                                        __attribute__((__cleanup__(__MCFCRT_SehTopUninstaller_x86)));	\
-	                                      __MCFCRT_SehTopInstaller_x86(&__MCFCRT_seh_node);	\
-	                                      {
+#  define __MCFCRT_SEH_TOP_BEGIN        { void *__aSehNode[2]	\
+                                            __attribute__((__cleanup__(__MCFCRT_SehTopUninstaller_x86)));	\
+                                            __MCFCRT_SehTopInstaller_x86(&__aSehNode);	\
+                                            {
 #  define __MCFCRT_SEH_TOP_END            }	\
-	                                    }
+                                        }
 #endif
 
 #endif

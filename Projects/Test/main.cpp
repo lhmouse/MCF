87ff7b4117da55d80908f1fa725317960b1a84e9
Liu Hao <lh_mouse@126.com>
2017-06-10 13:39:32 +0800

    .

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -2,13 +2,14 @@
 #include <cstdio>
 
 char str[100];
+volatile auto cp = std::memmove;
 
 extern "C" unsigned _MCFCRT_Main() noexcept {
 	std::memcpy(str, "abcdefghijklmnopqrstuvwxyz", 27);
-	std::memmove(str, str + 1, 10);
+	cp(str, str + 7, 19);
 	std::puts(str);
 	std::memcpy(str, "abcdefghijklmnopqrstuvwxyz", 27);
-	std::memmove(str + 1, str, 10);
+	cp(str + 7, str, 19);
 	std::puts(str);
 	return 0;
 }

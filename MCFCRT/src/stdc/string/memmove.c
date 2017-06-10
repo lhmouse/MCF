601fd2969856a88b6226a777e2359db5ca9bd632
Liu Hao <lh_mouse@126.com>
2017-06-10 13:31:16 +0800

    .

--- MCFCRT/src/stdc/string/memmove.c
+++ MCFCRT/src/stdc/string/memmove.c
@@ -3,11 +3,12 @@
 // Copyleft 2013 - 2032, LH_Mouse. All wrongs reserved.
 
 #include "_memcpy_impl.h"
+#include "../../env/expect.h"
 
 #undef memmove
 
 void *memmove(void *s1, const void *s2, size_t n){
-	if((uintptr_t)s1 < (uintptr_t)s2){
+	if(_MCFCRT_EXPECT((uintptr_t)s2 + n - (uintptr_t)s1 >= n)){
 		__MCFCRT_memcpy_impl_fwd(s1, s2, n);
 	} else {
 		__MCFCRT_memcpy_impl_bkwd(s1, s2, n);
--- MCFCRT/src/stdc/wchar/wmemmove.c
+++ MCFCRT/src/stdc/wchar/wmemmove.c
@@ -3,11 +3,12 @@
 // Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
 
 #include "../string/_memcpy_impl.h"
+#include "../../env/expect.h"
 
 #undef wmemmove
 
 wchar_t *wmemmove(wchar_t *restrict s1, const wchar_t *restrict s2, size_t n){
-	if((uintptr_t)s1 < (uintptr_t)s2){
+	if(_MCFCRT_EXPECT((uintptr_t)s2 + n - (uintptr_t)s1 >= n)){
 		__MCFCRT_memcpy_impl_fwd(s1, s2, sizeof(wchar_t) * n);
 	} else {
 		__MCFCRT_memcpy_impl_bkwd(s1, s2, sizeof(wchar_t) * n);

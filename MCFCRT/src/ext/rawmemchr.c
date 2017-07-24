27cfa55fdfd6e3e62c6fced4756db764ddd0598b
Liu Hao <lh_mouse@126.com>
2017-07-24 23:11:17 +0800

    Optimize more.

--- MCFCRT/src/ext/rawmemchr.c
+++ MCFCRT/src/ext/rawmemchr.c
@@ -11,7 +11,7 @@ void *_MCFCRT_rawmemchr(const void *s, int c){
 	// 如果 rp 是对齐到字的，就不用考虑越界的问题。
 	// 因为内存按页分配的，也自然对齐到页，并且也对齐到字。
 	// 每个字内的字节的权限必然一致。
-	while(((uintptr_t)rp & 15) != 0){
+	while(((uintptr_t)rp & 31) != 0){
 #define CHR_GEN()	\
 		{	\
 			const char rc = *rp;	\
@@ -26,13 +26,16 @@ void *_MCFCRT_rawmemchr(const void *s, int c){
 	{	\
 		const __m128i xc = _mm_set1_epi8((char)c);	\
 		for(;;){	\
-			const __m128i xw = _mm_load_si128((const __m128i *)rp);	\
-			__m128i xt = _mm_cmpeq_epi8(xw, xc);	\
+			const __m128i xw0 = _mm_load_si128((const __m128i *)rp + 0);	\
+			const __m128i xw1 = _mm_load_si128((const __m128i *)rp + 1);	\
+			__m128i xt = _mm_cmpeq_epi8(xw0, xc);	\
 			uint32_t mask = (uint32_t)_mm_movemask_epi8(xt);	\
+			xt = _mm_cmpeq_epi8(xw1, xc);	\
+			mask += (uint32_t)_mm_movemask_epi8(xt) << 16;	\
 			if(_MCFCRT_EXPECT_NOT(mask != 0)){	\
 				return (char *)rp + __builtin_ctzl(mask);	\
 			}	\
-			rp += 16;	\
+			rp += 32;	\
 		}	\
 	}
 	CHR_SSE3()

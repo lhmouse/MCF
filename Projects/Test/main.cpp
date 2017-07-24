32e11b1a4d963bf56eb879d638e13675a2badce2
Liu Hao <lh_mouse@126.com>
2017-07-24 16:10:53 +0800

    Update test code.

--- Projects/Test/main.cpp
+++ Projects/Test/main.cpp
@@ -5,39 +5,40 @@
 #include <MCF/Core/String.hpp>
 #include <MCF/Core/CopyMoveFill.hpp>
 
+using namespace MCF;
+
+struct PageDeleter {
+	constexpr void *operator()() const noexcept {
+		return nullptr;
+	}
+	void operator()(void *p) const noexcept {
+		::VirtualFree(p, 0, MEM_RELEASE);
+	}
+};
+
+using Char = wchar_t;
+
+constexpr std::size_t kSize = 0x200F000;
+
 extern "C" unsigned _MCFCRT_Main(void) noexcept {
-	using namespace MCF;
-
-	using Char = char;
-
-	struct PageDeleter {
-		constexpr void *operator()() const noexcept {
-			return nullptr;
-		}
-		void operator()(void *p) const noexcept {
-			::VirtualFree(p, 0, MEM_RELEASE);
-		}
-	};
-	constexpr std::size_t kSize = 0x200F000;
 	const UniquePtr<void, PageDeleter> p1(::VirtualAlloc(nullptr, kSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
 	const UniquePtr<void, PageDeleter> p2(::VirtualAlloc(nullptr, kSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE));
 	const auto s1  = (Char *)((char *)p1.Get() + 2);
 	const auto s1e = (Char *)((char *)p1.Get() + kSize);
+	Fill(s1, s1e, 'a')[-1] = 0;
 	const auto s2  = (Char *)((char *)p2.Get() + 18);
 	const auto s2e = (Char *)((char *)p2.Get() + kSize);
-
-	Fill(s1, s1e, 'a')[-1] = 0;
 	Fill(s2, s2e, 'a')[-1] = 0;
 
 	const auto test = [&](WideStringView name){
-		const auto fname = "strlen"_nsv;
+		const auto fname = "wcsncmp"_nsv;
 		try {
 			const DynamicLinkLibrary dll(name);
-			const auto pf = dll.RequireProcAddress<std::size_t (*)(const Char *)>(fname);
+			const auto pf = dll.RequireProcAddress<int (*)(const wchar_t *, const wchar_t *, std::size_t)>(fname);
 			std::intptr_t r;
 			const auto t1 = GetHiResMonoClock();
 			for(unsigned i = 0; i < 100; ++i){
-				r = (std::intptr_t)(*pf)(s2);
+				r = (std::intptr_t)(*pf)(s1, s2, kSize / sizeof(*s1));
 			}
 			const auto t2 = GetHiResMonoClock();
 			std::printf("%-10s.%s : t2 - t1 = %f, r = %td\n", AnsiString(name).GetStr(), AnsiString(fname).GetStr(), t2 - t1, r);

25d75cb3c9980c621a17096ccf5be4550b5a3d94
Liu Hao <lh_mouse@126.com>
2017-07-26 23:34:16 +0800

    Use the general version of `memset()`.

--- MCFCRT/src/stdc/string/memset.c
+++ MCFCRT/src/stdc/string/memset.c
@@ -3,29 +3,11 @@
 // Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
 
 #include "../../env/_crtdef.h"
-#include "../../ext/rep_stos.h"
+#include "../../env/inline_mem.h"
 
 #undef memset
 
 void *memset(void *s, int c, size_t n){
-	register char *wp = s;
-	char *const wend = wp + n;
-	uint8_t b = (uint8_t)c;
-#ifdef _WIN64
-	size_t nq = (size_t)(wend - wp) / 8;
-	uint64_t q = b;
-	q += (q <<  8);
-	q += (q << 16);
-	q += (q << 32);
-	wp = (void *)_MCFCRT_rep_stosq((void *)wp, q, nq);
-#else
-	size_t nd = (size_t)(wend - wp) / 4;
-	uint32_t d = b;
-	d += (d <<  8);
-	d += (d << 16);
-	wp = (void *)_MCFCRT_rep_stosd((void *)wp, d, nd);
-#endif
-	size_t nb = (size_t)(wend - wp);
-	wp = (void *)_MCFCRT_rep_stosb((void *)wp, b, nb);
+	_MCFCRT_inline_mempset_fwd(s, c, n);
 	return s;
 }

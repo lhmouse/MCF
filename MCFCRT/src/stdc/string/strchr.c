e73d725ef89a487a9f7cef24b3a3c075f98632e6
Liu Hao <lh_mouse@126.com>
2017-07-27 18:35:02 +0800

    Optimize it a little.

--- MCFCRT/src/stdc/string/strchr.c
+++ MCFCRT/src/stdc/string/strchr.c
@@ -24,8 +24,8 @@ char *strchr(const char *s, int c){
 		uint32_t mask;
 		__MCFCRT_xmmload_2(xw, rp, _mm_load_si128);
 		mask = __MCFCRT_xmmcmp_21b(xw, xz, _mm_cmpeq_epi8) & skip;
+		uint32_t zskip = (mask & -mask) * 2 - 1;
 		if(_MCFCRT_EXPECT_NOT(mask != 0)){
-			uint32_t zskip = (mask & -mask) * 2 - 1;
 			mask = __MCFCRT_xmmcmp_21b(xw, xc, _mm_cmpeq_epi8) & skip;
 			mask &= zskip;
 			if(_MCFCRT_EXPECT_NOT(mask != 0)){
--- MCFCRT/src/stdc/wchar/wcschr.c
+++ MCFCRT/src/stdc/wchar/wcschr.c
@@ -24,8 +24,8 @@ wchar_t *wcschr(const wchar_t *s, wchar_t c){
 		uint32_t mask;
 		__MCFCRT_xmmload_4(xw, rp, _mm_load_si128);
 		mask = __MCFCRT_xmmcmp_41w(xw, xz, _mm_cmpeq_epi16) & skip;
+		uint32_t zskip = (mask & -mask) * 2 - 1;
 		if(_MCFCRT_EXPECT_NOT(mask != 0)){
-			uint32_t zskip = (mask & -mask) * 2 - 1;
 			mask = __MCFCRT_xmmcmp_41w(xw, xc, _mm_cmpeq_epi16) & skip;
 			mask &= zskip;
 			if(_MCFCRT_EXPECT_NOT(mask != 0)){

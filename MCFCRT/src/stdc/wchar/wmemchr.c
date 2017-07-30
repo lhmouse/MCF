8a090ba5c20700f1e157142a31b9b96d6f5ac78e
Liu Hao <lh_mouse@126.com>
2017-07-30 21:51:09 +0800

    Tweaks.

--- MCFCRT/src/stdc/wchar/wmemchr.c
+++ MCFCRT/src/stdc/wchar/wmemchr.c
@@ -15,17 +15,17 @@ wchar_t *wmemchr(const wchar_t *s, wchar_t c, size_t n){
 	// 如果 rp 是对齐到字的，就不用考虑越界的问题。
 	// 因为内存按页分配的，也自然对齐到页，并且也对齐到字。
 	// 每个字内的字节的权限必然一致。
-	register const wchar_t *rp = (const wchar_t *)((uintptr_t)s & (uintptr_t)-64);
+	register const uint16_t *rp = (const uint16_t *)((uintptr_t)s & (uintptr_t)-64);
 	__m128i xc[1];
 	__MCFCRT_xmmsetw(xc, (uint16_t)c);
-	unsigned shift = (unsigned)((const wchar_t *)s - rp);
+	unsigned shift = (unsigned)((const uint16_t *)s - rp);
 	uint32_t skip = (uint32_t)-1 << shift;
 	for(;;){
 		__m128i xw[4];
 		uint32_t mask;
 		rp = __MCFCRT_xmmload_4(xw, rp, _mm_load_si128);
 		mask = __MCFCRT_xmmcmp_41w(xw, xc, _mm_cmpeq_epi16) & skip;
-		ptrdiff_t dist = rp - ((const wchar_t *)s + n);
+		ptrdiff_t dist = rp - ((const uint16_t *)s + n);
 		dist &= ~dist >> (sizeof(dist) * 8 - 1);
 		mask |= ~((uint32_t)-1 >> dist);
 		__builtin_prefetch(rp + 64, 0, 0);

9a6d4f408fc55f1ff6c2d470dab0ccb03c20d476
Liu Hao <lh_mouse@126.com>
2017-08-26 13:52:21 +0800

    For the backward version, copy (the very likely) unaligned bytes before the other ones.

--- MCFCRT/src/stdc/string/_memcpy_impl.h
+++ MCFCRT/src/stdc/string/_memcpy_impl.h
@@ -42,7 +42,7 @@ static inline void __MCFCRT_memcpy_small_fwd(void *__s1, const void *__s2, _MCFC
 			} while(_MCFCRT_EXPECT(__rem != 0));
 		}
 	}
-	__rem = (_MCFCRT_STD size_t)(((const unsigned char *)__s1 + __n) - __wp);
+	__rem = __n % sizeof(_MCFCRT_STD uintptr_t);
 	if(_MCFCRT_EXPECT(__rem != 0)){
 		switch(__rem % 8){
 			do {
@@ -70,7 +70,28 @@ void __MCFCRT_memcpy_large_fwd(void *__s1, const void *__s2, _MCFCRT_STD size_t 
 static inline void __MCFCRT_memcpy_small_bwd(_MCFCRT_STD size_t __n, void *__s1, const void *__s2) _MCFCRT_NOEXCEPT {
 	unsigned char *__wp = (unsigned char *)__s1;
 	const unsigned char *__rp = (const unsigned char *)__s2;
-	size_t __rem = __n / sizeof(_MCFCRT_STD uintptr_t);
+	size_t __rem = __n % sizeof(_MCFCRT_STD uintptr_t);
+	if(_MCFCRT_EXPECT(__rem != 0)){
+		switch(__rem % 8){
+			do {
+#define __MCFCRT_STEP_	\
+				*(volatile unsigned char *)(--__wp) = *(--__rp);	\
+				--__rem;
+//=============================================================================
+		__attribute__((__fallthrough__)); default: __MCFCRT_STEP_
+		__attribute__((__fallthrough__)); case 7:  __MCFCRT_STEP_
+		__attribute__((__fallthrough__)); case 6:  __MCFCRT_STEP_
+		__attribute__((__fallthrough__)); case 5:  __MCFCRT_STEP_
+		__attribute__((__fallthrough__)); case 4:  __MCFCRT_STEP_
+		__attribute__((__fallthrough__)); case 3:  __MCFCRT_STEP_
+		__attribute__((__fallthrough__)); case 2:  __MCFCRT_STEP_
+		__attribute__((__fallthrough__)); case 1:  __MCFCRT_STEP_
+//=============================================================================
+#undef __MCFCRT_STEP_
+			} while(_MCFCRT_EXPECT_NOT(__rem != 0));
+		}
+	}
+	__rem = __n / sizeof(_MCFCRT_STD uintptr_t);
 	if(_MCFCRT_EXPECT_NOT(__rem != 0)){
 		switch(__rem % 8){
 			_MCFCRT_STD uintptr_t __w;
@@ -93,27 +114,6 @@ static inline void __MCFCRT_memcpy_small_bwd(_MCFCRT_STD size_t __n, void *__s1,
 //=============================================================================
 #undef __MCFCRT_STEP_
 			} while(_MCFCRT_EXPECT(__rem != 0));
-		}
-	}
-	__rem = (_MCFCRT_STD size_t)(__wp - ((const unsigned char *)__s1 - __n));
-	if(_MCFCRT_EXPECT(__rem != 0)){
-		switch(__rem % 8){
-			do {
-#define __MCFCRT_STEP_	\
-				*(volatile unsigned char *)(--__wp) = *(--__rp);	\
-				--__rem;
-//=============================================================================
-		__attribute__((__fallthrough__)); default: __MCFCRT_STEP_
-		__attribute__((__fallthrough__)); case 7:  __MCFCRT_STEP_
-		__attribute__((__fallthrough__)); case 6:  __MCFCRT_STEP_
-		__attribute__((__fallthrough__)); case 5:  __MCFCRT_STEP_
-		__attribute__((__fallthrough__)); case 4:  __MCFCRT_STEP_
-		__attribute__((__fallthrough__)); case 3:  __MCFCRT_STEP_
-		__attribute__((__fallthrough__)); case 2:  __MCFCRT_STEP_
-		__attribute__((__fallthrough__)); case 1:  __MCFCRT_STEP_
-//=============================================================================
-#undef __MCFCRT_STEP_
-			} while(_MCFCRT_EXPECT_NOT(__rem != 0));
 		}
 	}
 }

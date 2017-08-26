fea9fa52c68bc7168f15eb02645531da611bed51
Liu Hao <lh_mouse@126.com>
2017-08-26 15:43:47 +0800

    Tweaks.

--- MCFCRT/src/stdc/string/_memcpy_impl.h
+++ MCFCRT/src/stdc/string/_memcpy_impl.h
@@ -45,28 +45,19 @@ static inline void __MCFCRT_memcpy_small_fwd(void *__s1, const void *__s2, _MCFC
 		}
 	}
 	__rem = __n % sizeof(_MCFCRT_STD uintptr_t);
-	if(_MCFCRT_EXPECT(__rem != 0)){
-		switch(__rem % 8){
-			do {
-#define __MCFCRT_STEP_(__l_)	\
-				__attribute__((__fallthrough__));	\
-		__l_:	\
-				*(volatile unsigned char *)(__wp++) = *(__rp++);	\
-				--__rem;
+#define __MCFCRT_STEP_(__b_)	\
+	if(_MCFCRT_EXPECT(__rem & (__b_ / 8))){	\
+		*(volatile _MCFCRT_STD uint ## __b_ ## _t *)__wp = *(_MCFCRT_STD uint ## __b_ ## _t *)__rp;	\
+		__wp += __b_ / 8;	\
+		__rp += __b_ / 8;	\
+	}	\
 //=============================================================================
-		__MCFCRT_STEP_(default)
-		__MCFCRT_STEP_(case  7)
-		__MCFCRT_STEP_(case  6)
-		__MCFCRT_STEP_(case  5)
-		__MCFCRT_STEP_(case  4)
-		__MCFCRT_STEP_(case  3)
-		__MCFCRT_STEP_(case  2)
-		__MCFCRT_STEP_(case  1)
+	__MCFCRT_STEP_(64)
+	__MCFCRT_STEP_(32)
+	__MCFCRT_STEP_(16)
+	__MCFCRT_STEP_( 8)
 //=============================================================================
 #undef __MCFCRT_STEP_
-			} while(_MCFCRT_EXPECT_NOT(__rem != 0));
-		}
-	}
 }
 
 void __MCFCRT_memcpy_large_fwd(void *__s1, const void *__s2, _MCFCRT_STD size_t __n) _MCFCRT_NOEXCEPT;
@@ -75,28 +66,19 @@ static inline void __MCFCRT_memcpy_small_bwd(_MCFCRT_STD size_t __n, void *__s1,
 	unsigned char *__wp = (unsigned char *)__s1;
 	const unsigned char *__rp = (const unsigned char *)__s2;
 	size_t __rem = __n % sizeof(_MCFCRT_STD uintptr_t);
-	if(_MCFCRT_EXPECT(__rem != 0)){
-		switch(__rem % 8){
-			do {
-#define __MCFCRT_STEP_(__l_)	\
-				__attribute__((__fallthrough__));	\
-		__l_:	\
-				*(volatile unsigned char *)(--__wp) = *(--__rp);	\
-				--__rem;
+#define __MCFCRT_STEP_(__b_)	\
+	if(_MCFCRT_EXPECT(__rem & (__b_ / 8))){	\
+		__wp -= __b_ / 8;	\
+		__rp -= __b_ / 8;	\
+		*(volatile _MCFCRT_STD uint ## __b_ ## _t *)__wp = *(_MCFCRT_STD uint ## __b_ ## _t *)__rp;	\
+	}	\
 //=============================================================================
-		__MCFCRT_STEP_(default)
-		__MCFCRT_STEP_(case  7)
-		__MCFCRT_STEP_(case  6)
-		__MCFCRT_STEP_(case  5)
-		__MCFCRT_STEP_(case  4)
-		__MCFCRT_STEP_(case  3)
-		__MCFCRT_STEP_(case  2)
-		__MCFCRT_STEP_(case  1)
+	__MCFCRT_STEP_(64)
+	__MCFCRT_STEP_(32)
+	__MCFCRT_STEP_(16)
+	__MCFCRT_STEP_( 8)
 //=============================================================================
 #undef __MCFCRT_STEP_
-			} while(_MCFCRT_EXPECT_NOT(__rem != 0));
-		}
-	}
 	__rem = __n / sizeof(_MCFCRT_STD uintptr_t);
 	if(_MCFCRT_EXPECT_NOT(__rem != 0)){
 		switch(__rem % 8){

9ce076430522b14c55550c37c8a0eea56ea19659
Liu Hao <lh_mouse@126.com>
2017-08-26 16:06:01 +0800

    Optimize more.

--- MCFCRT/src/stdc/string/_memcpy_impl.h
+++ MCFCRT/src/stdc/string/_memcpy_impl.h
@@ -7,6 +7,7 @@
 
 #include "../../env/_crtdef.h"
 #include "../../env/expect.h"
+#include <emmintrin.h>
 
 _MCFCRT_EXTERN_C_BEGIN
 
@@ -17,18 +18,16 @@ _MCFCRT_CONSTEXPR bool __MCFCRT_memcpy_is_small_enough(_MCFCRT_STD size_t __n) _
 static inline void __MCFCRT_memcpy_small_fwd(void *__s1, const void *__s2, _MCFCRT_STD size_t __n) _MCFCRT_NOEXCEPT {
 	unsigned char *__wp = (unsigned char *)__s1;
 	const unsigned char *__rp = (const unsigned char *)__s2;
-	size_t __rem = __n / sizeof(_MCFCRT_STD uintptr_t);
+	size_t __rem = __n / 8;
 	if(_MCFCRT_EXPECT_NOT(__rem != 0)){
 		switch(__rem % 8){
-			_MCFCRT_STD uintptr_t __w;
 			do {
 #define __MCFCRT_STEP_(__l_)	\
 				__attribute__((__fallthrough__));	\
 		__l_:	\
-				__builtin_memcpy(&__w, __rp, sizeof(__w));	\
-				__builtin_memcpy(__wp, &__w, sizeof(__w));	\
-				__wp += sizeof(__w);	\
-				__rp += sizeof(__w);	\
+				_mm_storel_epi64((__m128i *)__wp, _mm_loadl_epi64((const __m128i *)__rp));	\
+				__wp += 8;	\
+				__rp += 8;	\
 				--__rem;
 //=============================================================================
 		__MCFCRT_STEP_(default)
@@ -44,7 +43,7 @@ static inline void __MCFCRT_memcpy_small_fwd(void *__s1, const void *__s2, _MCFC
 			} while(_MCFCRT_EXPECT(__rem != 0));
 		}
 	}
-	__rem = __n % sizeof(_MCFCRT_STD uintptr_t);
+	__rem = __n % 8;
 #define __MCFCRT_STEP_(__b_)	\
 	if(_MCFCRT_EXPECT(__rem & (__b_ / 8))){	\
 		*(volatile _MCFCRT_STD uint ## __b_ ## _t *)__wp = *(_MCFCRT_STD uint ## __b_ ## _t *)__rp;	\
@@ -52,7 +51,6 @@ static inline void __MCFCRT_memcpy_small_fwd(void *__s1, const void *__s2, _MCFC
 		__rp += __b_ / 8;	\
 	}	\
 //=============================================================================
-	__MCFCRT_STEP_(64)
 	__MCFCRT_STEP_(32)
 	__MCFCRT_STEP_(16)
 	__MCFCRT_STEP_( 8)
@@ -65,7 +63,7 @@ void __MCFCRT_memcpy_large_fwd(void *__s1, const void *__s2, _MCFCRT_STD size_t 
 static inline void __MCFCRT_memcpy_small_bwd(_MCFCRT_STD size_t __n, void *__s1, const void *__s2) _MCFCRT_NOEXCEPT {
 	unsigned char *__wp = (unsigned char *)__s1;
 	const unsigned char *__rp = (const unsigned char *)__s2;
-	size_t __rem = __n % sizeof(_MCFCRT_STD uintptr_t);
+	size_t __rem = __n % 8;
 #define __MCFCRT_STEP_(__b_)	\
 	if(_MCFCRT_EXPECT(__rem & (__b_ / 8))){	\
 		__wp -= __b_ / 8;	\
@@ -73,24 +71,21 @@ static inline void __MCFCRT_memcpy_small_bwd(_MCFCRT_STD size_t __n, void *__s1,
 		*(volatile _MCFCRT_STD uint ## __b_ ## _t *)__wp = *(_MCFCRT_STD uint ## __b_ ## _t *)__rp;	\
 	}	\
 //=============================================================================
-	__MCFCRT_STEP_(64)
 	__MCFCRT_STEP_(32)
 	__MCFCRT_STEP_(16)
 	__MCFCRT_STEP_( 8)
 //=============================================================================
 #undef __MCFCRT_STEP_
-	__rem = __n / sizeof(_MCFCRT_STD uintptr_t);
+	__rem = __n / 8;
 	if(_MCFCRT_EXPECT_NOT(__rem != 0)){
 		switch(__rem % 8){
-			_MCFCRT_STD uintptr_t __w;
 			do {
 #define __MCFCRT_STEP_(__l_)	\
 				__attribute__((__fallthrough__));	\
 		__l_:	\
-				__wp -= sizeof(__w);	\
-				__rp -= sizeof(__w);	\
-				__builtin_memcpy(&__w, __rp, sizeof(__w));	\
-				__builtin_memcpy(__wp, &__w, sizeof(__w));	\
+				__wp += 8;	\
+				__rp += 8;	\
+				_mm_storel_epi64((__m128i *)__wp, _mm_loadl_epi64((const __m128i *)__rp));	\
 				--__rem;
 //=============================================================================
 		__MCFCRT_STEP_(default)

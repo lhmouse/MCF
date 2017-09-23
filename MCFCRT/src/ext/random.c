019bd0c75325d5365e50c3bf8e11018e97eedd6a
Liu Hao <lh_mouse@126.com>
2017-09-24 00:06:19 +0800

    Naming.

--- MCFCRT/src/ext/random.c
+++ MCFCRT/src/ext/random.c
@@ -6,13 +6,13 @@
 #include "../env/clocks.h"
 
 uint32_t _MCFCRT_GetRandomUint32(void){
-	static volatile uint64_t s_u64Seed = 0;
-	uint64_t u64OldSeed, u64Seed;
-	u64OldSeed = __atomic_load_n(&s_u64Seed, __ATOMIC_RELAXED);
+	static volatile uint64_t s_u64SeedNew = 0;
+	uint64_t u64SeedOld, u64SeedNew;
+	u64SeedOld = __atomic_load_n(&s_u64SeedNew, __ATOMIC_RELAXED);
 	do {
-		u64Seed = (u64OldSeed ^ _MCFCRT_ReadTimeStampCounter32()) * 6364136223846793005u + 1442695040888963407u;
-	} while(!__atomic_compare_exchange_n(&s_u64Seed, &u64OldSeed, u64Seed, false, __ATOMIC_RELAXED, __ATOMIC_RELAXED));
-	return (uint32_t)(u64Seed >> 32);
+		u64SeedNew = (u64SeedOld ^ _MCFCRT_ReadTimeStampCounter32()) * 6364136223846793005u + 1442695040888963407u;
+	} while(!__atomic_compare_exchange_n(&s_u64SeedNew, &u64SeedOld, u64SeedNew, false, __ATOMIC_RELAXED, __ATOMIC_RELAXED));
+	return (uint32_t)(u64SeedNew >> 32);
 }
 uint64_t _MCFCRT_GetRandomUint64(void){
 	return ((uint64_t)_MCFCRT_GetRandomUint32() << 32) | _MCFCRT_GetRandomUint32();

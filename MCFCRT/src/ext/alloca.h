c0c295a5d927c03120b4e608dcc7034ec46e0897
Liu Hao <lh_mouse@126.com>
2017-09-30 21:49:54 +0800

    Minor.

--- MCFCRT/src/ext/alloca.h
+++ MCFCRT/src/ext/alloca.h
@@ -8,7 +8,7 @@
 #include "../env/_crtdef.h"
 
 #define _MCFCRT_ALLOCA(__size_)	\
-	(__extension__({	\
+	(__extension__ ({	\
 		const _MCFCRT_STD size_t __size = (__size_);	\
 		void *const __ptr = __builtin_alloca(__size);	\
 		for(_MCFCRT_STD size_t __offset = 0xFF0; __offset < __size; __offset += 0x1000){	\

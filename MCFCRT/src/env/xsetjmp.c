94631fbb303203a176113ff9cc3066d4952cf041
Liu Hao <lh_mouse@126.com>
2017-11-13 09:54:54 +0800

    Add xsetjmp.{h,c}.

---
+++ MCFCRT/src/env/xsetjmp.c
@@ -0,0 +1,10 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
+
+#include "xsetjmp.h"
+
+__attribute__((__noreturn__))
+void __MCFCRT_longjmp_wrapper(_MCFCRT_jmp_buf env){
+	__builtin_longjmp(env, 1);
+}
---
+++ MCFCRT/src/env/xsetjmp.h
@@ -0,0 +1,24 @@
+// 这个文件是 MCF 的一部分。
+// 有关具体授权说明，请参阅 MCFLicense.txt。
+// Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
+
+#ifndef __MCFCRT_ENV_XSETJMP_H_
+#define __MCFCRT_ENV_XSETJMP_H_
+
+#include "_crtdef.h"
+
+_MCFCRT_EXTERN_C_BEGIN
+
+typedef struct __MCFCRT_tag_jmp_buf {
+	_MCFCRT_STD uintptr_t __a[5];
+} _MCFCRT_jmp_buf[1];
+
+__attribute__((__noreturn__))
+extern void __MCFCRT_longjmp_wrapper(_MCFCRT_jmp_buf __env) _MCFCRT_NOEXCEPT;
+
+#define _MCFCRT_SETJMP(__env_)    __builtin_setjmp((void **)(__env_))
+#define _MCFCRT_LONGJMP(__env_)   __MCFCRT_longjmp_wrapper((void **)(__env_))
+
+_MCFCRT_EXTERN_C_END
+
+#endif

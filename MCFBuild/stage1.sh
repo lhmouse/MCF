b9038c881145b09581cca08c2dbcfc20796a691b
Liu Hao <lh_mouse@126.com>
2017-11-23 23:34:55 +0800

    Update stage1.sh

--- MCFBuild/stage1.sh
+++ MCFBuild/stage1.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-CC=gcc
-DEST=../_Build/stage1
+_cc="gcc"
+_dest="../_Build/stage1"
 
-rm -rf "${DEST}/"
+if [[ -n "${OPTIMIZE}" ]]; then
+  _opt_flags="-O3"
+else
+  _opt_flags="-O0"
+fi
 
-mkdir -p "${DEST}/bin/"
-mkdir -p "${DEST}/lib/"
-mkdir -p "${DEST}/include/mcfbuild/"
+rm -rf "${_dest}/"
 
-"${CC}" -std=c11 -Wall -Wextra -pedantic -pedantic-errors -Werror \
+mkdir -p "${_dest}/bin/"
+mkdir -p "${_dest}/lib/"
+mkdir -p "${_dest}/include/mcfbuild/"
+
+"${_cc}" -std=c11 -Wall -Wextra -pedantic -pedantic-errors -Werror \
   -Wno-error=unused-parameter -Winvalid-pch -Wwrite-strings -Wconversion \
   -Wsign-conversion -Wdouble-promotion -Wsuggest-attribute=noreturn \
   -Wundef -Wshadow -Wstrict-aliasing=2 -Wstrict-prototypes \
   -pipe -mfpmath=both -march=core2 -mtune=intel -municode \
   -shared -Wl,--export-all-symbols,--exclude-libs,ALL \
   -Wl,--disable-stdcall-fixup,--enable-auto-image-base \
-  -O0 -g3 $(find . -name "*.c") \
-  -o "${DEST}/bin/mcfbuild-stage1.dll" \
-  -Wl,--out-implib,"${DEST}/lib/libmcfbuild.dll.a"
+  "${_opt_flags}" -g3 $(find . -name "*.c") \
+  -o "${_dest}/bin/mcfbuild-stage1.dll" \
+  -Wl,--out-implib,"${_dest}/lib/libmcfbuild.dll.a"
 cp -Rp $(find . -name "*.h") \
-  -t "${DEST}/include/mcfbuild/" --parents
+  -t "${_dest}/include/mcfbuild/" --parents

a2c9d52c2a79f4935d7f96294de10dcb62d1cb37
Liu Hao <lh_mouse@126.com>
2017-11-23 22:45:31 +0800

    Add a line of comment in heap.h.

--- MCFBuild/heap.h
+++ MCFBuild/heap.h
@@ -9,6 +9,8 @@
 
 MCFBUILD_EXTERN_C_BEGIN
 
+// These functions set the last error code to `ERROR_NOT_ENOUGH_MEMORY` on failure.
+
 extern void *MCFBUILD_HeapAlloc(MCFBUILD_STD size_t uSize) MCFBUILD_NOEXCEPT;
 extern void *MCFBUILD_HeapRealloc(void *pBlockOld, MCFBUILD_STD size_t uSize) MCFBUILD_NOEXCEPT;
 extern void MCFBUILD_HeapFree(void *pBlock) MCFBUILD_NOEXCEPT;

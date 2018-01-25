5368554967316e7891511d8bf36d86b1e51288e2
Liu Hao <lh_mouse@126.com>
2018-01-25 16:45:21 +0800

    Cleanup.

--- MCFCRT/src/stdc/string/memcmp.c
+++ MCFCRT/src/stdc/string/memcmp.c
@@ -5,47 +5,48 @@
 #include "../../env/_crtdef.h"
 #include "../../env/expect.h"
 
+#define UINTPTR_BYTES     (sizeof(uintptr_t))
+
 #undef memcmp
 
 int memcmp(const void *s1, const void *s2, size_t n){
 	const unsigned char *rp1 = s1;
 	const unsigned char *rp2 = s2;
-	size_t rem = n / sizeof(uintptr_t);
-	if(_MCFCRT_EXPECT_NOT(rem != 0)){
-		switch((rem - 1) % 32){
-			uintptr_t w, c;
+	size_t cnt = n / UINTPTR_BYTES;
+	switch(cnt % 32){
+		uintptr_t w, c;
 #define STEP(k_)	\
-				__attribute__((__fallthrough__));	\
-		case (k_):	\
-				__builtin_memcpy(&w, rp1, sizeof(w));	\
-				__builtin_memcpy(&c, rp2, sizeof(c));	\
-				if(_MCFCRT_EXPECT_NOT(w != c)){	\
-					rem = sizeof(uintptr_t);	\
-					goto diff_char;	\
-				}	\
-				rp1 += sizeof(w);	\
-				rp2 += sizeof(c);	\
-				--rem;
+			__builtin_memcpy(&w, rp1, UINTPTR_BYTES);	\
+			__builtin_memcpy(&c, rp2, UINTPTR_BYTES);	\
+			if(_MCFCRT_EXPECT_NOT(w != c)){	\
+				cnt = UINTPTR_BYTES;	\
+				goto diff_byte;	\
+			}	\
+			rp1 += UINTPTR_BYTES;	\
+			rp2 += UINTPTR_BYTES;	\
+			--cnt;	\
+			__attribute__((__fallthrough__));	\
+	case (k_):	\
+			;
 //=============================================================================
-			do {
-		STEP(037)  STEP(036)  STEP(035)  STEP(034)  STEP(033)  STEP(032)  STEP(031)  STEP(030)
-		STEP(027)  STEP(026)  STEP(025)  STEP(024)  STEP(023)  STEP(022)  STEP(021)  STEP(020)
-		STEP(017)  STEP(016)  STEP(015)  STEP(014)  STEP(013)  STEP(012)  STEP(011)  STEP(010)
-		STEP(007)  STEP(006)  STEP(005)  STEP(004)  STEP(003)  STEP(002)  STEP(001)  STEP(000)
-			} while(_MCFCRT_EXPECT(rem != 0));
+		do {
+	STEP(037)  STEP(036)  STEP(035)  STEP(034)  STEP(033)  STEP(032)  STEP(031)  STEP(030)
+	STEP(027)  STEP(026)  STEP(025)  STEP(024)  STEP(023)  STEP(022)  STEP(021)  STEP(020)
+	STEP(017)  STEP(016)  STEP(015)  STEP(014)  STEP(013)  STEP(012)  STEP(011)  STEP(010)
+	STEP(007)  STEP(006)  STEP(005)  STEP(004)  STEP(003)  STEP(002)  STEP(001)  STEP(000)
+		} while(_MCFCRT_EXPECT(cnt != 0));
 //=============================================================================
 #undef STEP
-		}
 	}
-	rem = n % sizeof(uintptr_t);
-diff_char:
-	while(_MCFCRT_EXPECT(rem != 0)){
+	cnt = n % UINTPTR_BYTES;
+diff_byte:
+	while(_MCFCRT_EXPECT(cnt != 0)){
 		if(*rp1 != *rp2){
 			return (*rp1 < *rp2) ? -1 : 1;
 		}
 		++rp1;
 		++rp2;
-		--rem;
+		--cnt;
 	}
 	return 0;
 }
--- MCFCRT/src/stdc/wchar/wmemcmp.c
+++ MCFCRT/src/stdc/wchar/wmemcmp.c
@@ -5,47 +5,48 @@
 #include "../../env/_crtdef.h"
 #include "../../env/expect.h"
 
+#define UINTPTR_WCHARS     (sizeof(uintptr_t) / sizeof(wchar_t))
+
 #undef wmemcmp
 
 int wmemcmp(const wchar_t *s1, const wchar_t *s2, size_t n){
 	const wchar_t *rp1 = s1;
 	const wchar_t *rp2 = s2;
-	size_t rem = n / (sizeof(uintptr_t) / sizeof(wchar_t));
-	if(_MCFCRT_EXPECT_NOT(rem != 0)){
-		switch((rem - 1) % 32){
-			uintptr_t w, c;
+	size_t cnt = n / UINTPTR_WCHARS;
+	switch(cnt % 32){
+		uintptr_t w, c;
 #define STEP(k_)	\
-				__attribute__((__fallthrough__));	\
-		case (k_):	\
-				__builtin_memcpy(&w, rp1, sizeof(w));	\
-				__builtin_memcpy(&c, rp2, sizeof(c));	\
-				if(_MCFCRT_EXPECT_NOT(w != c)){	\
-					rem = sizeof(uintptr_t) / sizeof(wchar_t);	\
-					goto diff_wchar;	\
-				}	\
-				rp1 += sizeof(w) / sizeof(wchar_t);	\
-				rp2 += sizeof(c) / sizeof(wchar_t);	\
-				--rem;
+			__builtin_memcpy(&w, rp1, UINTPTR_WCHARS);	\
+			__builtin_memcpy(&c, rp2, UINTPTR_WCHARS);	\
+			if(_MCFCRT_EXPECT_NOT(w != c)){	\
+				cnt = UINTPTR_WCHARS;	\
+				goto diff_wchar;	\
+			}	\
+			rp1 += UINTPTR_WCHARS;	\
+			rp2 += UINTPTR_WCHARS;	\
+			--cnt;	\
+			__attribute__((__fallthrough__));	\
+	case (k_):	\
+			;
 //=============================================================================
-			do {
-		STEP(037)  STEP(036)  STEP(035)  STEP(034)  STEP(033)  STEP(032)  STEP(031)  STEP(030)
-		STEP(027)  STEP(026)  STEP(025)  STEP(024)  STEP(023)  STEP(022)  STEP(021)  STEP(020)
-		STEP(017)  STEP(016)  STEP(015)  STEP(014)  STEP(013)  STEP(012)  STEP(011)  STEP(010)
-		STEP(007)  STEP(006)  STEP(005)  STEP(004)  STEP(003)  STEP(002)  STEP(001)  STEP(000)
-			} while(_MCFCRT_EXPECT(rem != 0));
+		do {
+	STEP(037)  STEP(036)  STEP(035)  STEP(034)  STEP(033)  STEP(032)  STEP(031)  STEP(030)
+	STEP(027)  STEP(026)  STEP(025)  STEP(024)  STEP(023)  STEP(022)  STEP(021)  STEP(020)
+	STEP(017)  STEP(016)  STEP(015)  STEP(014)  STEP(013)  STEP(012)  STEP(011)  STEP(010)
+	STEP(007)  STEP(006)  STEP(005)  STEP(004)  STEP(003)  STEP(002)  STEP(001)  STEP(000)
+		} while(_MCFCRT_EXPECT(cnt != 0));
 //=============================================================================
 #undef STEP
-		}
 	}
-	rem = n % (sizeof(uintptr_t) / sizeof(wchar_t));
+	cnt = n % UINTPTR_WCHARS;
 diff_wchar:
-	while(_MCFCRT_EXPECT(rem != 0)){
+	while(_MCFCRT_EXPECT(cnt != 0)){
 		if(*rp1 != *rp2){
 			return (*rp1 < *rp2) ? -1 : 1;
 		}
 		++rp1;
 		++rp2;
-		--rem;
+		--cnt;
 	}
 	return 0;
 }

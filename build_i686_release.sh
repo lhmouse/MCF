82bb279625b820174efcec4a08cc81e96dcbd01a
Liu Hao <lh_mouse@126.com>
2018-01-27 14:07:50 +0800

    Use `-O2` in place of `-O3`.

--- build_i686_release.sh
+++ build_i686_release.sh
@@ -6,9 +6,9 @@ prefix="$(pwd)/release/mingw32"
 mkdir -p "$prefix"
 
 export CPPFLAGS+=" -I$prefix/include -DNDEBUG"
-export CFLAGS+=" -O3 -ffunction-sections -fdata-sections"
-export CXXFLAGS+=" -O3 -ffunction-sections -fdata-sections"
-export LDFLAGS+=" -O3 -Wl,-s,--gc-sections -L$prefix/lib"
+export CFLAGS+=" -O2 -ffunction-sections -fdata-sections"
+export CXXFLAGS+=" -O2 -ffunction-sections -fdata-sections"
+export LDFLAGS+=" -O2 -Wl,-s,--gc-sections -L$prefix/lib"
 
 builddir="$(pwd)/.build_i686_release"
 build=i686-w64-mingw32
